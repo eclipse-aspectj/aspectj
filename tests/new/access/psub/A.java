fdda168314476e94315ee2b844c2ff55ea944ddc
jhugunin <jhugunin>
2002-12-18 06:17:05 +0000

    added coverage for this(InaccessibleTypeToOthers)

--- tests/new/access/psub/A.java
+++ tests/new/access/psub/A.java
@@ -4,10 +4,12 @@ import org.aspectj.testing.Tester;
 import pc.C;
 
 aspect A {
+	private static interface Marker {}
+	
     before(): call(String SubC.getMyPackage()) {
 	//XXXTester.checkFailed("shouldn't ever run this " + thisJoinPoint);
     }
-    before(): call(String C.getMyPackage()) {
+    before(): !target(Marker) && !this(Marker) && !args(Marker, ..) && call(String C.getMyPackage()) {
 	Tester.note("C.getMyPackage on " + thisJoinPoint.getTarget().getClass().getName());
     }
 }
