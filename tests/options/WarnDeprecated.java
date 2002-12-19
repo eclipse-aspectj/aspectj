c3002200cae795108f88fd5f7949dfa6f31a1616
jhugunin <jhugunin>
2002-12-19 22:43:10 +0000

    using external deprecated method to get warning

--- tests/options/WarnDeprecated.java
+++ tests/options/WarnDeprecated.java
@@ -4,10 +4,10 @@
 
 public class WarnDeprecated {
 
-    /** @deprecated */
+    /** */
     public static void main(String[] args) {
        if (null == args) {
-            main(new String[0]); // CE 10 deprecated if warn:deprecated
+            String s = new String(new byte[] {}, 0); // CE 10 deprecated if warn:deprecated
        }
     }
 }
