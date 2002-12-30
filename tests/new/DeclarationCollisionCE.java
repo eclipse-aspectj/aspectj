4b069263be1a381592bb521eb0211e84f37dbcfe
jhugunin <jhugunin>
2002-12-30 19:18:26 +0000

    fixed to use declare dominates

--- tests/new/DeclarationCollisionCE.java
+++ tests/new/DeclarationCollisionCE.java
@@ -10,21 +10,21 @@ public class DeclarationCollisionCE {
 class C {}
 
 // bug: dominates clause prevents collision error
-abstract aspect AA dominates B { 
+abstract aspect AA { declare dominates: AA, B; 
     // same result if this line is uncommented
     //public String C.title() { return "[AA] C.title()"; }
 }
 
 aspect A extends AA { // implicitly dominates AA
     // dominates AA's declaration, overriding Super.title
-    public String C.title() {           // CE 21 collision with B declaration
+    public String C.title() {           // CE 20 collision with B declaration
         return "[A] C.title()" ; 
     }
 }
 
 aspect B extends AA { // explicitly dominated by AA ?? --> and hence by A??
     // B fails to dominate AA's declaration, overriding Super.title
-    public String C.title() {           // CE 28 collision with A declaration
+    public String C.title() {           // CE 27 collision with A declaration
         return "[B] C.title()" ; 
     }
 }
