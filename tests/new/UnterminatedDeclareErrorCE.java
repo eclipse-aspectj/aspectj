922a42c30dc7a8dcbf17bce1973c87fd0b46e3f3
jhugunin <jhugunin>
2002-12-30 19:14:37 +0000

    fixed expected error line

--- tests/new/UnterminatedDeclareErrorCE.java
+++ tests/new/UnterminatedDeclareErrorCE.java
@@ -1,8 +1,8 @@
 
 /** @testcase expect CE for unterminated declare error */
 aspect UnterminatedDeclareErrorCE {
-    declare error : execution(void run()) : "error" // CE 7
-}
+    declare error : execution(void run()) : "error" 
+}// CE 5
 
 class C {
     // bug - get CE here if uncommented, but declare error unterminated
