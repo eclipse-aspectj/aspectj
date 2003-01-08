29dccb0d7ec7786bb219cbdf02e3bb81fcd843bb
jhugunin <jhugunin>
2003-01-08 02:17:09 +0000

    added version not in 'aspect' package

---
+++ tests/new/PR852/aspectPack/Aspect.java
@@ -0,0 +1,15 @@
+package aspectPack;
+
+import target.SuperClass;
+import target.SubClass;
+
+/** @testcase PR#852 declaring method on superclass and subclass */
+public aspect Aspect {
+    void SuperClass.test() { System.out.println("SuperClass ok");}
+    void SubClass.test() { System.out.println("SubClass ok");}
+    //public void SuperClass+.callTest() { test(); }
+    public static void main (String[] args) {
+        new SuperClass().test();
+        new SubClass().test();
+    }
+}
