fefd4559fb4a9eefcf4528526ad2448b9a6e3489
jhugunin <jhugunin>
2003-04-22 17:35:11 +0000

    fix for Bugzilla Bug 36736    compiler error expected for implemented abstract pointcuts
    and for
    
    fix for Bugzilla Bug 36673  
       VerifyError in privileged aspect 
    minimally extended coverage for this bug

--- tests/new/verifyError/Privilege.java
+++ tests/new/verifyError/Privilege.java
@@ -7,13 +7,20 @@ class C {
 
 privileged aspect A {
 	private int C.j = 1;
+	private static String C.s = "hello";
+	
+	private String C.m() {
+		return "from A";
+	}
 }
 
 /** @testcase PR#36673 privileged aspect main verify error */
 public privileged aspect Privilege {
 	public static void main(String[] args) {
 	    C c = new C();
-		Tester.check(1 == c.j, "wrong value for c.j");
+		Tester.checkEqual(1, c.j, "wrong value for c.j");
+		Tester.checkEqual("hello", C.s, "wrong value for C.s");
+		Tester.checkEqual("from A", c.m(), "c.m()");
 	}	
 }
 
