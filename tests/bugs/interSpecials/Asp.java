8d793a3f9960f101a4cae59b959ce612f3fef4c9
jhugunin <jhugunin>
2003-04-27 23:30:14 +0000

    tests for Bugzilla Bug 36936    Error when introducing members of type Class

---
+++ tests/bugs/interSpecials/Asp.java
@@ -0,0 +1,3 @@
+public aspect Asp { 
+	public Class Trg.member = String.class; 
+} 
---
+++ tests/bugs/interSpecials/Trg.java
@@ -0,0 +1,6 @@
+public class Trg { 
+	public static void main(String args[]) { 
+		new Trg(); 
+		System.out.println("All ok"); 
+	} 
+} 
