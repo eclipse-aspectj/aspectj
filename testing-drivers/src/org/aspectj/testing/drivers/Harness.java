d5fb5792dde297d8b21c3538cc871d3d26f1876b
wisberg <wisberg>
2003-05-25 20:10:59 +0000

    harness copyright wrong - was created in 2002

--- testing-drivers/src/org/aspectj/testing/drivers/Harness.java
+++ testing-drivers/src/org/aspectj/testing/drivers/Harness.java
@@ -1,6 +1,6 @@
 /* *******************************************************************
- * Copyright (c) 1999-2001 Xerox Corporation, 
- *               2002 Palo Alto Research Center, Incorporated (PARC).
+ * Copyright (c) 2002 Palo Alto Research Center, Incorporated (PARC),
+ *               2003 Contributors.
  * All rights reserved. 
  * This program and the accompanying materials are made available 
  * under the terms of the Common Public License v1.0 
@@ -9,6 +9,7 @@
  *  
  * Contributors: 
  *     Xerox/PARC     initial implementation 
+ *     Wes Isberg     2003 changes.
  * ******************************************************************/
 
 package org.aspectj.testing.drivers;
