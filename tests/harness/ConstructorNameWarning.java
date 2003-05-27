11c67124a2f9a01214a91165d36c9a7bc512052b
wisberg <wisberg>
2003-05-27 07:30:12 +0000

    test checks that setting warn:constructorName works

---
+++ tests/harness/ConstructorNameWarning.java
@@ -0,0 +1,9 @@
+
+public class ConstructorNameWarning {
+    
+    /** expecting warning: method with constructor name */
+    public void ConstructorNameWarning(String s) { // CW 5 -warn:constructorName
+    }
+}
+
+
