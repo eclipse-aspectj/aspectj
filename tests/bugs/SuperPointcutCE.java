10fb3c602d53d02377e80b45687cbbf0096d71dc
wisberg <wisberg>
2003-07-28 21:50:11 +0000

    @testcase PR#40858 weaver trace on mis-qualified pointcut reference

---
+++ tests/bugs/SuperPointcutCE.java
@@ -0,0 +1,27 @@
+
+
+public class MissingTypeSigatureCE {
+    public static void main(String[] a) {
+        new C().run();
+    }
+}
+
+class C {
+    public void run(){ System.out.println("c");}
+}
+
+abstract aspect AA {
+    pointcut pc() : call(public * *(..)) && !within(AA+);
+    before() : pc() {
+        System.out.println("here: " + thisJoinPointStaticPart);
+    }
+}
+
+/** @testcase PR#40858 weaver trace on mis-qualified pointcut reference */
+aspect B extends AA {
+    
+    pointcut pc() : super.pc() 
+        && !call(void println(..));
+        
+    pointcut blah() : UnknownType.pc();  // CE
+}
