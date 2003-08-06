40fb45135a9079752db1fc3a984d278d054c6dbb
wisberg <wisberg>
2003-08-06 00:29:13 +0000

    @testcase PR#41170 combine classpath on command line and in .lst file

---
+++ tests/bugs/options/classpath/Client.java
@@ -0,0 +1,6 @@
+
+public class Client {
+    public static void main(String[] args) {
+        lib.Library.method();
+    }
+}
---
+++ tests/bugs/options/classpath/lib/Library.java
@@ -0,0 +1,7 @@
+
+package lib;
+
+public class Library {
+    public static void method() {
+    }
+}
