c19c596d276457bb01d6f9e48311a6e3e290c7b4
ehilsdal <ehilsdal>
2003-08-28 16:51:45 +0000

    moved to List

--- docs/teaching/exercises/support/Log.java
+++ docs/teaching/exercises/support/Log.java
@@ -1,45 +1,47 @@
 /* *******************************************************************
  * Copyright (c) 2002 Palo Alto Research Center, Incorporated (PARC).
- * All rights reserved. 
- * This program and the accompanying materials are made available 
- * under the terms of the Common Public License v1.0 
- * which accompanies this distribution and is available at 
- * http://www.eclipse.org/legal/cpl-v10.html 
- *  
- * Contributors: 
- *     PARC     initial implementation 
+ * All rights reserved.
+ * This program and the accompanying materials are made available
+ * under the terms of the Common Public License v1.0
+ * which accompanies this distribution and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *
+ * Contributors:
+ *     PARC     initial implementation
  * ******************************************************************/
 
 
 package support;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Log {
-    static StringBuffer data = new StringBuffer();
+    static List data = new ArrayList();
 
     public static void traceObject(Object o) {
         throw new UnsupportedOperationException();
     }
 
     public static void log(String s) {
-        data.append(s);
-        data.append(';');
+        data.add(s);
     }
 
-    public static void logClassName(Class _class) {
-        String name = _class.getName();
-        int dot = name.lastIndexOf('.');
-        if (dot == -1) {
-            log(name);
-        } else {
-            log(name.substring(dot+1, name.length()));
-        }
-    }
+//     public static void logClassName(Class _class) {
+//         String name = _class.getName();
+//         int dot = name.lastIndexOf('.');
+//         if (dot == -1) {
+//             log(name);
+//         } else {
+//             log(name.substring(dot+1, name.length()));
+//         }
+//     }
 
-    public static String getString() {
-        return data.toString();
+    public static List getData() {
+        return data;
     }
 
     public static void clear() {
-        data.setLength(0);
+        data.clear();
     }
 }
