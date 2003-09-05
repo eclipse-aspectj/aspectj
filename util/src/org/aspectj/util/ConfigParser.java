365ba016e1a854591ec2e872b050a40174a453eb
mkersten <mkersten>
2003-09-05 06:49:08 +0000

    Exposed file creation to allow overriding (for the use of canonicalization).

--- util/src/org/aspectj/util/ConfigParser.java
+++ util/src/org/aspectj/util/ConfigParser.java
@@ -205,7 +205,7 @@ public class ConfigParser {
         return makeFile(getCurrentDir(), name);
     }
 
-    File makeFile(File dir, String name) {
+    protected File makeFile(File dir, String name) {
         name = name.replace('/', File.separatorChar);
         File ret = new File(name);
         if (dir == null || ret.isAbsolute()) return ret;
