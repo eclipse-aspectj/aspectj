c5537bf1f37d27f39ca729803f2d664167b3792d
aclement <aclement>
2004-04-07 16:05:07 +0000

    Moved from the other package.

---
+++ ajdoc/testsrc/AjdocModuleTests.java
@@ -0,0 +1,32 @@
+/* *******************************************************************
+ * Copyright (c) 2003 Contributors.
+ * All rights reserved. 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Common Public License v1.0 
+ * which accompanies this distribution and is available at 
+ * http://www.eclipse.org/legal/cpl-v10.html 
+ *  
+ * Contributors: 
+ *     Mik Kersten     initial implementation 
+ * ******************************************************************/
+import org.aspectj.tools.ajdoc.CoverageTestCase;
+import org.aspectj.tools.ajdoc.ExecutionTestCase;
+import org.aspectj.tools.ajdoc.SpacewarTestCase;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Mik Kersten
+ */
+public class AjdocModuleTests {
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for org.aspectj.tools.ajdoc");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(SpacewarTestCase.class);
+		suite.addTestSuite(CoverageTestCase.class); 
+		suite.addTestSuite(ExecutionTestCase.class);// !!! must be last because it exists
+		//$JUnit-END$
+		return suite;
+	}
+}
