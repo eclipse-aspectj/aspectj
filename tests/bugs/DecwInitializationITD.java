591e8fd6d22f308af87ed766884e4e5e8ceaba09
aclement <aclement>
2004-04-26 09:48:34 +0000

    Test for Bugzilla Bug 59397    NPE in compiler when using (an unusual) declare warning against a ctor ITD

---
+++ tests/bugs/DecwInitializationITD.java
@@ -0,0 +1,16 @@
+
+// The actual ctor that gets put in type 'HW' during compilation is
+// HW(String,A)
+// This is an artifact of the implementation of ctor ITDing - unfortunately this artifact
+// can be seen by the declare warning.  So,
+//  initialization(HW.new(..))  will match our new ctor
+//  initialization(HW.new(String,A)) will match our new ctor
+//  initialization(HW.new(String)) will not match !
+
+aspect A {  
+	HW.new(String s) {}  
+	declare warning : initialization(HW.new(String,A)) : "Funky ctor found!";
+}
+
+
+class HW {}
