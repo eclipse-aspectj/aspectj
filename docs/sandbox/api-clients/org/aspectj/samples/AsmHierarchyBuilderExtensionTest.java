5130e9dbb7ad9b806475355d8c2861eb57085656
mkersten <mkersten>
2004-07-27 17:34:34 +0000

    ASM Usage and extension examples, implemented as test cases and made available in the sandbox as an Eclipse project.

---
+++ docs/sandbox/api-clients/org/aspectj/samples/AsmHierarchyBuilderExtensionTest.java
@@ -0,0 +1,67 @@
+/* *******************************************************************
+ * Copyright (c) 2004 Contributors.
+ * All rights reserved. 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Common Public License v1.0 
+ * which accompanies this distribution and is available at 
+ * http://www.eclipse.org/legal/cpl-v10.html 
+ *  
+ * Contributors: 
+ *     Mik Kersten     initial implementation 
+ * ******************************************************************/
+ package org.aspectj.samples;
+
+import java.util.ArrayList;
+
+import org.aspectj.ajde.AjdeTestCase;
+import org.aspectj.ajdt.internal.core.builder.AjBuildManager;
+import org.aspectj.ajdt.internal.core.builder.AsmHierarchyBuilder;
+import org.aspectj.asm.AsmManager;
+import org.aspectj.asm.IProgramElement;
+import org.aspectj.asm.internal.ProgramElement;
+import org.eclipse.jdt.internal.compiler.ast.MessageSend;
+import org.eclipse.jdt.internal.compiler.lookup.BlockScope;
+
+/**
+ * This test demonstrates how hierarchy building in the ASM can be extended
+ * to put additional information in the model, for example method call sites.
+ * 
+ * @author Mik Kersten
+ */
+public class AsmHierarchyBuilderExtensionTest extends AjdeTestCase {
+
+    private ExtendedAsmHiearchyBuilder builder = new ExtendedAsmHiearchyBuilder();
+    
+	public void testHiearchyExtension() {
+	    assertNotNull(AsmManager.getDefault().getHierarchy().getRoot());
+	    System.out.println(AsmManager.getDefault().getHierarchy().getRoot().toLongString());
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp("examples");
+		AjBuildManager.setAsmHierarchyBuilder(builder);  // NOTE that we set our builder here
+		assertTrue("build success", doSynchronousBuild("../examples/coverage/coverage.lst"));	
+	}
+}
+
+class ExtendedAsmHiearchyBuilder extends AsmHierarchyBuilder {
+    
+    public boolean visit(MessageSend messageSend, BlockScope scope) {
+		IProgramElement peNode = new ProgramElement(
+				new String(">>> found call: " + messageSend.toString()),
+				IProgramElement.Kind.CODE,	
+				null, //makeLocation(messageSend),
+				0,
+				"",
+				new ArrayList());
+//			peNode.setCorrespondingType(messageSend.typ  ieldDeclaration.type.toString());
+//			peNode.setSourceSignature(genSourceSignature(fieldDeclaration));
+			((IProgramElement)stack.peek()).addChild(peNode);
+			stack.push(peNode);
+			return true;
+    }
+    public void endVisit(MessageSend messageSend, BlockScope scope) {
+        stack.pop();
+    }
+
+}
---
+++ docs/sandbox/api-clients/org/aspectj/samples/AsmRelationshipMapUsageTest.java
@@ -0,0 +1,81 @@
+/* *******************************************************************
+ * Copyright (c) 2004 Contributors.
+ * All rights reserved. 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Common Public License v1.0 
+ * which accompanies this distribution and is available at 
+ * http://www.eclipse.org/legal/cpl-v10.html 
+ *  
+ * Contributors: 
+ *     Mik Kersten     initial implementation 
+ * ******************************************************************/
+
+package org.aspectj.samples;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.aspectj.ajde.AjdeTestCase;
+import org.aspectj.ajdt.internal.core.builder.AjBuildManager;
+import org.aspectj.asm.*;
+
+/**
+ * Collects join point information for all advised methods and constructors.  
+ * 
+ * @author Mik Kersten
+ */
+public class AsmRelationshipMapUsageTest extends AjdeTestCase {
+
+    public void testFindAdvisedMethods() {        
+        System.out.println("----------------------------------");
+        System.out.println("Methods affected by advice: ");
+        HierarchyWalker walker = new HierarchyWalker() {
+            public void preProcess(IProgramElement node) {
+                if (node.getKind().equals(IProgramElement.Kind.METHOD)) {
+                    List relations = AsmManager.getDefault().getRelationshipMap().get(node);
+                    if (relations != null) {
+	                    for (Iterator it = relations.iterator(); it.hasNext(); ) {
+	                        IRelationship relationship = (IRelationship)it.next();
+	                        if (relationship.getKind().equals(IRelationship.Kind.ADVICE)) {
+	                            System.out.println(
+	                                    "method: " + node.toString() 
+	                                    + ", advised by: " + relationship.getTargets());
+	                        } 
+	                    }
+                    }
+                }
+            }
+        };
+        AsmManager.getDefault().getHierarchy().getRoot().walk(walker);
+    }
+    
+    public void testListFilesAffectedByInterTypeDecs() {
+        System.out.println("----------------------------------");
+        System.out.println("Files affected by inter type declarations: ");
+        HierarchyWalker walker = new HierarchyWalker() {
+            public void preProcess(IProgramElement node) {
+                if (node.getKind().equals(IProgramElement.Kind.CLASS)) {
+                    List relations = AsmManager.getDefault().getRelationshipMap().get(node);
+                    if (relations != null) {
+	                    for (Iterator it = relations.iterator(); it.hasNext(); ) {
+	                        IRelationship relationship = (IRelationship)it.next();
+	                        if (relationship.getKind().equals(IRelationship.Kind.DECLARE_INTER_TYPE)) {
+	                            System.out.println(
+                                    "file: " + node.getSourceLocation().getSourceFile().getName() 
+                                    + ", declared on by: " + relationship.getTargets());
+	                        } 
+	                    }
+                    }
+                }
+            }
+        };
+        AsmManager.getDefault().getHierarchy().getRoot().walk(walker);
+    }
+    
+	
+	protected void setUp() throws Exception {
+	    super.setUp("examples");
+		assertTrue("build success", doSynchronousBuild("../examples/spacewar/spacewar/debug.lst"));	
+	}
+}
+
