22a8bfca49914270469791b409d4d63d3bb83be2
wisberg <wisberg>
2004-10-14 23:13:31 +0000

    set static variable to avoid reading value set by other tests

--- tests/harness/aspectpath/Main.java
+++ tests/harness/aspectpath/Main.java
@@ -1,5 +1,8 @@
 
 public class Main {
+    static {
+        System.setProperty("A.before", "false");
+    }
     public static void main(String[] args) {
         if (!Boolean.getBoolean("A.before")) {
             throw new Error("property A.before not set by aspect A.java");
