3197038957ee0a4d0228ad03c54f644347b9386a
aclement <aclement>
2005-02-02 10:46:46 +0000

    java5 project represents stuff that needs Java5 to compile (currently contains only additions to the 'runtime' project)

---
+++ java5/runtime-src/org/aspectj/lang/annotation/Around.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2005 Jonas Bonér, Alexandre Vasseur
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *******************************************************************************/
+package org.aspectj.lang.annotation;
+
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+/**
+ * Around advice
+ *
+ * @author <a href="mailto:alex AT gnilux DOT com">Alexandre Vasseur</a>
+ */
+@Target(ElementType.METHOD)
+public @interface Around {
+
+    /**
+     * The pointcut expression where to bind the advice
+     */
+    String value();
+}
