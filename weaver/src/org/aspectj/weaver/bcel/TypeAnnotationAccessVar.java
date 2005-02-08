70d7c0de3083547dccbcacdc62e5b91142c3a4d7
aclement <aclement>
2005-02-08 09:53:27 +0000

    Fix copyright.

--- weaver/src/org/aspectj/weaver/bcel/TypeAnnotationAccessVar.java
+++ weaver/src/org/aspectj/weaver/bcel/TypeAnnotationAccessVar.java
@@ -1,5 +1,5 @@
 /* *******************************************************************
- * Copyright (c) 2005 
+ * Copyright (c) 2005 IBM
  * All rights reserved. 
  * This program and the accompanying materials are made available 
  * under the terms of the Common Public License v1.0 
