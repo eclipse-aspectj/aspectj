2e7d6a211402509c429a4ca6c5e0517e711eeadc
aclement <aclement>
2005-02-17 13:31:49 +0000

    Didnt have a default.

--- aspectj5rt/java5-src/org/aspectj/lang/annotation/SuppressAjWarnings.java
+++ aspectj5rt/java5-src/org/aspectj/lang/annotation/SuppressAjWarnings.java
@@ -27,5 +27,5 @@ import java.lang.annotation.RetentionPolicy;
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SuppressAjWarnings {
-  String[] value();
+  String[] value() default "";
 }
