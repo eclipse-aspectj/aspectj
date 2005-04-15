8c7470d4139ab6a21b87f419bcf2a79cfb93cea7
aclement <aclement>
2005-04-15 12:20:47 +0000

    Moved copyright stuff around

--- aspectj5rt/java5-src/org/aspectj/lang/annotation/Before.java
+++ aspectj5rt/java5-src/org/aspectj/lang/annotation/Before.java
@@ -1,9 +1,12 @@
 /*******************************************************************************
- * Copyright (c) Jonas Bonér, Alexandre Vasseur
+ * Copyright (c) 2005 Contributors.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *   Jonas Bonér, Alexandre Vasseur         initial implementation
  *******************************************************************************/
 package org.aspectj.lang.annotation;
 
@@ -15,7 +18,7 @@ import java.lang.annotation.ElementType;
  *
  * @author <a href="mailto:alex AT gnilux DOT com">Alexandre Vasseur</a>
  */
-//FIXME @AJ RV or RIV for @AJ
+//FIXME av @AJ annotations RuntimeVisible or RuntimeInvisible? Applies to all annotations in this package
 @Target(ElementType.METHOD)
 public @interface Before {
 
