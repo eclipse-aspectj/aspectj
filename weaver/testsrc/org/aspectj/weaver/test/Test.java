4b65a28214b84794ff1b0720897346d9ed40858a
aclement <aclement>
2005-04-15 12:03:09 +0000

    copyrights added.

--- weaver/testsrc/org/aspectj/weaver/test/Test.java
+++ weaver/testsrc/org/aspectj/weaver/test/Test.java
@@ -1,3 +1,13 @@
+/* Copyright (c) 2002 Contributors.
+ * 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Common Public License v1.0 
+ * which accompanies this distribution and is available at 
+ * http://www.eclipse.org/legal/cpl-v10.html 
+ *  
+ * Contributors: 
+ *     PARC     initial implementation 
+ */
 package org.aspectj.weaver.test;
 
 public class Test {
