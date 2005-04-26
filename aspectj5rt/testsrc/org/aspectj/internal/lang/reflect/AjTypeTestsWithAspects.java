9da1384f0cd0e5f2da94a05ba1c57e60a83a93d8
aclement <aclement>
2005-04-26 16:02:30 +0000

    Copyright added to keep checklics happy.

--- aspectj5rt/testsrc/org/aspectj/internal/lang/reflect/AjTypeTestsWithAspects.java
+++ aspectj5rt/testsrc/org/aspectj/internal/lang/reflect/AjTypeTestsWithAspects.java
@@ -1,10 +1,18 @@
-package org.aspectj.internal.lang.reflect;
+/* *******************************************************************
+ * Copyright (c) 2005 Contributors.
+ * All rights reserved. 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution and is available at 
+ * http://eclipse.org/legal/epl-v10.html 
+ *  
+ * Contributors: 
+ *     Adrian Colyer       initial implementation 
+ * ******************************************************************/package org.aspectj.internal.lang.reflect;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.EnumSet;
-import java.util.Set;
 
 import junit.framework.TestCase;
 
