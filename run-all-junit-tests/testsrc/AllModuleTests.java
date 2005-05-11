a3d3d71c8d720a41502fe131c0297241790ceeb9
wisberg <wisberg>
2005-05-11 09:45:33 +0000

    Name picked up by Ant junit task

---
+++ run-all-junit-tests/testsrc/AllModuleTests.java
@@ -0,0 +1,21 @@
+/* *******************************************************************
+ * Copyright (c) 2005 Contributors.
+ * All rights reserved. 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution and is available at 
+ * http://eclipse.org/legal/epl-v10.html 
+ *  
+ * Contributors: 
+ *     Wes Isberg       initial implementation 
+ * ******************************************************************/
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+public class AllModuleTests extends TestCase {
+    public static Test suite() {
+        return RunTheseBeforeYouCommitTests.suite();
+    }
+
+}
