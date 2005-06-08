9db52cb41e1eaf83c6f281cd24b6cac634e5c3e1
wisberg <wisberg>
2005-06-08 23:56:24 +0000

    Module alias does not include compiler tests since tests/ does

--- run-all-junit-tests/testsrc/AllModuleTests.java
+++ run-all-junit-tests/testsrc/AllModuleTests.java
@@ -13,9 +13,14 @@
 import junit.framework.Test;
 import junit.framework.TestCase;
 
+/**
+ * Alias to maintain convention for invoking JUnit from Ant
+ * by discovery of module root tests that name is *ModuleTests.
+ */
 public class AllModuleTests extends TestCase {
     public static Test suite() {
-        return RunTheseBeforeYouCommitTests.suite();
+        // does not include compiler tests, i.e., tests/../TestsModuleTests
+        return AllTests.suite();
     }
 
 }
