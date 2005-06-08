0c4e822ebe1aed1e24e271e4496d5ace41570168
wisberg <wisberg>
2005-06-08 23:34:33 +0000

    Skipping long test of rarely-changed code

--- testing-drivers/testsrc/org/aspectj/testing/drivers/HarnessSelectionTest.java
+++ testing-drivers/testsrc/org/aspectj/testing/drivers/HarnessSelectionTest.java
@@ -92,6 +92,8 @@ public class HarnessSelectionTest extends TestCase {
     }
 
     public void testIncrementalSuite() {
+        System.err.println("skippping testIncrementalSuite - too long");
+        if (true) return;
         if (!eclipseAvailable()) {
             System.err.println("skipping test - eclipse classes not available");
             return;
