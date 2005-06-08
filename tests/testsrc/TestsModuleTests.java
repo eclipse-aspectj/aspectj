ab64b605f3bb81f3852a81440969d54e20a10308
wisberg <wisberg>
2005-06-08 23:48:36 +0000

    testsrc/TestsModulesTests for consistency.  Now to run compiler tests, just `cd tests; ant test`

---
+++ tests/testsrc/TestsModuleTests.java
@@ -0,0 +1,42 @@
+/* *******************************************************************
+ * Copyright (c) 2005 Contributors.
+ * All rights reserved. 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution and is available at 
+ * http://eclipse.org/legal/epl-v10.html 
+ *  
+ * Contributors: 
+ *     Wes Isberg       initial implementation 
+ * ******************************************************************/
+
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.aspectj.systemtest.AllTests;
+import org.aspectj.systemtest.AllTests14;
+import org.aspectj.systemtest.AllTests15;
+import org.aspectj.util.LangUtil;
+
+public class TestsModuleTests extends TestCase {
+
+    public static Test suite() {
+        String name = TestsModuleTests.class.getName();
+        TestSuite suite = new TestSuite(name);
+        // compiler tests, wrapped for JUnit
+        suite.addTest(AllTests.suite());            
+        if (LangUtil.is15VMOrGreater()) {
+            suite.addTest(AllTests14.suite());            
+            suite.addTest(AllTests15.suite());
+        } else if (LangUtil.is14VMOrGreater()) {
+            System.err.println("Skipping tests for 1.5");
+            //suite.addTest(TestUtil.skipTest("for 1.5"));
+            suite.addTest(AllTests14.suite());            
+        } else {
+            System.err.println("Skipping tests for 1.4 and 1.5");
+            //suite.addTest(TestUtil.skipTest("for 1.4 and 1.5"));
+        }
+        return suite;
+    }
+}
