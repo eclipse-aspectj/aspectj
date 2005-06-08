f65bc76fdf72ca584b534ab8ae983583c0b241f1
wisberg <wisberg>
2005-06-08 23:35:21 +0000

    Java 5 guard to permit running in 1.3

--- aspectj5rt/testsrc/Aspectj5rtModuleTests.java
+++ aspectj5rt/testsrc/Aspectj5rtModuleTests.java
@@ -21,7 +21,11 @@ public class Aspectj5rtModuleTests extends TestCase {
 
     public static Test suite() {
         TestSuite suite = new TestSuite("Aspectj5rt module tests");
-        TestUtil.loadTestsReflectively(suite, "Aspectj5rt15ModuleTests", true);
+        if (TestUtil.is15VMOrGreater()) {
+            TestUtil.loadTestsReflectively(suite, "Aspectj5rt15ModuleTests", true);
+        } else {
+            suite.addTest(TestUtil.skipTest("for aspectj5rt that need 1.5"));
+        }
         return suite;
     }
 
--- loadtime5/testsrc/Loadtime5ModuleTests.java
+++ loadtime5/testsrc/Loadtime5ModuleTests.java
@@ -22,7 +22,11 @@ public class Loadtime5ModuleTests extends TestCase {
 
     public static Test suite() {
         TestSuite suite = new TestSuite(Loadtime5ModuleTests.class.getName());
-        TestUtil.loadTestsReflectively(suite, "Loadtime515ModuleTests", true);
+        if (TestUtil.is15VMOrGreater()) {
+            TestUtil.loadTestsReflectively(suite, "Loadtime515ModuleTests", true);
+        } else {
+            suite.addTest(TestUtil.testNamed("all tests require 1.5"));
+        }
         return suite;
     }
     public static void main(String[] args) {
