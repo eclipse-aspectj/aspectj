c84327365db294d051dcd32690481b0f0281d756
aclement <aclement>
2005-06-24 08:00:09 +0000

    licensing fix.

--- weaver/src/org/aspectj/weaver/TypeX.java
+++ weaver/src/org/aspectj/weaver/TypeX.java
@@ -1,5 +1,5 @@
 /* *******************************************************************
- * Copyright (c) 2002,2005
+ * Copyright (c) 2002,2005 Contributors
  * All rights reserved. 
  * This program and the accompanying materials are made available 
  * under the terms of the Common Public License v1.0 
