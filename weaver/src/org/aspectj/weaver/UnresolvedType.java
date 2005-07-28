c804cb81a9a903df09d8ddeae4365d352d5ebc5a
aclement <aclement>
2005-07-28 10:27:09 +0000

    organized imports

--- weaver/src/org/aspectj/weaver/UnresolvedType.java
+++ weaver/src/org/aspectj/weaver/UnresolvedType.java
@@ -17,9 +17,6 @@ package org.aspectj.weaver;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 import org.aspectj.apache.bcel.classfile.GenericSignatureParser;
