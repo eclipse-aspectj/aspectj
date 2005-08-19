b6534a87a3a6dc094bf2c704548f49094f250dc4
acolyer <acolyer>
2005-08-19 08:01:02 +0000

    collection of really basic tests to use when bringing up a new version of the JDT compiler (these are not linked into the main test suite). Fortunately I didn't need too many in here this time round...

---
+++ tests/bringup/EmptyAspect.aj
@@ -0,0 +1,1 @@
+public aspect EmptyAspect {}
---
+++ tests/src/org/aspectj/systemtest/bringup/BringUpTests.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2004 IBM 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *
+ * Contributors:
+ *    Andy Clement - initial API and implementation
+ *******************************************************************************/
+package org.aspectj.systemtest.bringup;
+
+import java.io.File;
+
+import junit.framework.Test;
+
+import org.aspectj.testing.XMLBasedAjcTestCase;
+
+/**
+ * These are tests that will run on Java 1.4 and use the old harness format for test specification.
+ */
+public class BringUpTests extends org.aspectj.testing.XMLBasedAjcTestCase {
+	  
+  public static Test suite() {
+    return XMLBasedAjcTestCase.loadSuite(BringUpTests.class);
+  }
+
+  protected File getSpecFile() {
+    return new File("../tests/src/org/aspectj/systemtest/bringup/bringup.xml");
+  }
+
+  public void testEmptyClass() {
+	  runTest("empty class");
+  }
+  
+  public void testEmptyAspect() {
+	  runTest("empty aspect");
+  }
+}
