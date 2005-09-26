0ea99cad049a88bb2465472f8eba9800ae9d44f3
aclement <aclement>
2005-09-26 15:36:21 +0000

    weaver5 split out so weaver can continue to be built with Java1.3

---
+++ weaver5/java5-testsrc/Weaver5ModuleTests.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2005 Contributors.
+ * All rights reserved.
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution and is available at
+ * http://eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: (See CVS logs)
+ * 
+ *******************************************************************************/
+
+import org.aspectj.testing.util.TestUtil;
+import org.aspectj.util.LangUtil;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ */
+public class Weaver5ModuleTests extends TestCase {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite(Weaver5ModuleTests.class.getName());
+        if (TestUtil.is15VMOrGreater()) {
+//        	if (LangUtil.is15VMOrGreater()) {
+	            TestUtil.loadTestsReflectively(suite, "org.aspectj.weaver.tools.Java15PointcutExpressionTest", false);
+//	        }
+//            TestUtil.loadTestsReflectively(suite, "Weaver515ModuleTests", true);
+        } else {
+            suite.addTest(TestUtil.testNamed("all tests require 1.5"));
+        }
+        return suite;
+    }
+    public static void main(String[] args) {
+        junit.textui.TestRunner.main(new String[] {Weaver5ModuleTests.class.getName()});
+    }
+
+}
