d7625fa4436befbbae170aec577cccc344705b91
aclement <aclement>
2005-10-07 10:07:25 +0000

    testcode for bug 111779 (commented out for now...)

--- tests/src/org/aspectj/systemtest/incremental/tools/MultiProjectIncrementalTests.java
+++ tests/src/org/aspectj/systemtest/incremental/tools/MultiProjectIncrementalTests.java
@@ -288,6 +288,14 @@ public class MultiProjectIncrementalTests extends AjdeInteractionTestbed {
 		build("PR92837");
 	}
 	
+/*	public void testPr111779() {
+		initialiseProject("PR111779");
+		build("PR111779");
+		alter("PR111779","inc1");
+		build("PR111779");
+	}
+*/
+	
 	// other possible tests:
 	// - memory usage (freemem calls?)
 	// - relationship map
