007a8005e8ba974ac840f0e2dc31431fb7bf0951
aclement <aclement>
2005-11-22 16:40:44 +0000

    117296: testcode

--- tests/bugs150/pr117296/PropertySupport.java
+++ tests/bugs150/pr117296/PropertySupport.java
@@ -1,3 +1,14 @@
 public class PropertySupport<T extends PropertySupport<T>> {
 
+  public static void main(String []argv) {
+  }
 }
+
+
+class Two {
+}
+
+aspect X {
+  declare parents: Two extends PropertySupport;
+}
+
