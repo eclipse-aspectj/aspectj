c31c8198b4a33f5319bffac5fa5121caec341aff
aclement <aclement>
2005-12-12 11:52:18 +0000

    more tests for 119570

---
+++ tests/multiIncremental/PR119570/base/bugs/ParameterizedDP.java
@@ -0,0 +1,27 @@
+package bugs;
+
+import bugsOtherPackage.INode;
+import bugsOtherPackage.NodeImpl;
+
+public class ParameterizedDP {
+
+    public static void main(String[] args) {
+        // 1) compile-time error here without 
+        //    {unneeded} subaspect declare-parent
+        // Tag as INode<Tag, Tag> from PC extends NodeImpl<Tag, Tag>
+        ((TaggedTexts.Tag) null).getParent();       
+    }
+
+}
+class TaggedTexts {
+
+    public static class Text {  }
+
+    public static class Tag {  }
+    static aspect PC extends NodeImpl<Tag, Tag> {
+        // unneeded declare-parents duplicates one in NodeImpl
+        // when here, get spurious error message
+        // when commented out, d-p fails and get compiler error at 1) above
+        declare parents : Tag implements INode<Tag,Tag>;
+    }
+}
