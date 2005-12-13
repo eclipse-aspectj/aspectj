514aaeaf764c99a54df01e46355d8c64b1696de5
acolyer <acolyer>
2005-12-13 16:52:03 +0000

    1.5 -> 1.3

--- weaver/testsrc/org/aspectj/weaver/tools/PointcutDesignatorHandlerTests.java
+++ weaver/testsrc/org/aspectj/weaver/tools/PointcutDesignatorHandlerTests.java
@@ -26,7 +26,7 @@ public class PointcutDesignatorHandlerTests extends TestCase {
 			  .parsePointcutExpression("bean(service.*");
 			fail("should not be able to parse bean(service.*)");
 		} catch(IllegalArgumentException ex) {
-			assertTrue("contains bean",ex.getMessage().contains("bean"));
+			assertTrue("contains bean",ex.getMessage().indexOf("bean") != -1);
 		}
 	}
 	
