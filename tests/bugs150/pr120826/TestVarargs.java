bb8a6c47fa01cd869076c39d6f52ffde83f7c3a0
aclement <aclement>
2005-12-14 11:06:03 +0000

    test for 120826

---
+++ tests/bugs150/pr120826/TestVarargs.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+//import junit.framework.TestCase;
+
+/**
+ * @author Pekka Enberg
+ */
+public class TestVarargs /*extends TestCase */{
+    public void testVarargs() throws Exception {
+        Arrays.asList(String.class, Long.class, Integer.class);
+    }
+}
