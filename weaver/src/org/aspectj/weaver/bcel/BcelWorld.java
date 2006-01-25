efb20c5a3eeecb85c059110b3494f7b39593ab28
aclement <aclement>
2006-01-25 17:41:21 +0000

    oops, crap comment earlier - fix for 125101

--- weaver/src/org/aspectj/weaver/bcel/BcelWorld.java
+++ weaver/src/org/aspectj/weaver/bcel/BcelWorld.java
@@ -307,6 +307,7 @@ public class BcelWorld extends World implements Repository {
         
         if (nameTypeX == null) {        	
 		    if (jc.isGeneric() && isInJava5Mode()) {
+		    
 		    	nameTypeX =  ReferenceType.fromTypeX(UnresolvedType.forRawTypeName(jc.getClassName()),this);
 		        ret = makeBcelObjectType(nameTypeX, jc, true);
 		    	ReferenceType genericRefType = new ReferenceType(
