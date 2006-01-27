a723cc27959dbb9b6e3eb3eea797163bd4ac7102
aclement <aclement>
2006-01-27 11:11:34 +0000

    testcode for 125405

---
+++ tests/multiIncremental/PR125405/base/test/ATest.java
@@ -0,0 +1,9 @@
+package test;
+
+public aspect ATest
+{
+   // main
+   public static void main(String[] args) {
+
+   }
+}
---
+++ tests/multiIncremental/PR125405/inc1/test/ATest.java
@@ -0,0 +1,9 @@
+package test;
+
+public aspect ATest<T>
+{
+   // main
+   public static void main(String[] args) {
+
+   }
+}
