abff16da00d0dd4f1aec75411c1ee40ec65c8278
acolyer <acolyer>
2006-02-10 17:57:49 +0000

    define some constants to avoid spreading string literals around

--- weaver/src/org/aspectj/weaver/IWeaver.java
+++ weaver/src/org/aspectj/weaver/IWeaver.java
@@ -21,4 +21,8 @@ package org.aspectj.weaver;
  */
 public interface IWeaver {
 
+	public static final String CLOSURE_CLASS_PREFIX = "$Ajc";
+	
+	public static final String SYNTHETIC_CLASS_POSTFIX = "$ajc";
+	
 }
