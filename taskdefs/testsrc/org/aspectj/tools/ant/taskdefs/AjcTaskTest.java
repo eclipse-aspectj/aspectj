08ed5064848a6a895dbd18fc9416abbb51dd1c69
aclement <aclement>
2006-02-21 09:26:13 +0000

    organized imports

--- taskdefs/testsrc/org/aspectj/tools/ant/taskdefs/AjcTaskTest.java
+++ taskdefs/testsrc/org/aspectj/tools/ant/taskdefs/AjcTaskTest.java
@@ -17,7 +17,6 @@
 package org.aspectj.tools.ant.taskdefs;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileReader;
