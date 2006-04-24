8c084b533078c9d640aa9d1530e673d744d546ad
aclement <aclement>
2006-04-24 14:58:47 +0000

    work in progress testcase 137568

--- tests/bugs152/pr137568/C.java
+++ tests/bugs152/pr137568/C.java
@@ -1,19 +1,29 @@
+//interface IGuard<P> {}
+//
+////interface Guard<P> extends IGuard<P> {}
+//
+//class GuardImpl<G> implements IGuard<G> {}
+//
+//public class C<T> {
+//
+//  private boolean m1(Class<? extends IGuard<T>> guardClz) throws Exception { return false;}
+//  private boolean m2(Class<? extends IGuard<T>>[] guardClz) throws Exception { return false;}
+//
+//  public static void main(String []argv) throws Exception {
+//    GuardImpl<String> g = new GuardImpl<String>();
+//    C<String> newC = new C<String>();
+//    newC.m1(g.getClass());
+////    newC.m2(new Class[]{g.getClass()});
+//  }
+//
+//}
+
 interface IGuard<P> {}
-
 interface Guard<P> extends IGuard<P> {}
-
-class GuardImpl implements Guard<String> {}
-
 public class C<T> {
-
-  private boolean m1(Class<? extends IGuard<T>> guardClz) throws Exception { return false;}
-  private boolean m2(Class<? extends IGuard<T>>[] guardClz) throws Exception { return false;}
-
-  public static void main(String []argv) throws Exception {
-    GuardImpl g = new GuardImpl();
-    C<String> newC = new C<String>();
-    newC.m1(g.getClass());
-    newC.m2(new Class[]{g.getClass()});
+  private boolean checkGuards(Class<? extends IGuard<T>>[] guardClz) throws Exception { return false;}
+  public static void main(String []argv) {
+    Guard<String> g = new Guard<String>();
+    new C<String>().checkGuards(g.getClass());//Guard.class);
   }
-
 }
