41340ae6cb640bf77b6e68cce2ff71e5aa982a2b
mwebster <mwebster>
2006-08-14 15:54:06 +0000

    Bug 150487 "Tracing and Logging Framework" (allow LTW IMessageHandler implementations to add context)

---
+++ weaver/src/org/aspectj/weaver/tools/ISupportsMessageContext.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Matthew Webster - initial implementation
+ *******************************************************************************/
+package org.aspectj.weaver.tools;
+
+import org.aspectj.bridge.IMessageContext;
+
+public interface ISupportsMessageContext {
+
+	public void setMessageContext (IMessageContext messageContext);
+}
