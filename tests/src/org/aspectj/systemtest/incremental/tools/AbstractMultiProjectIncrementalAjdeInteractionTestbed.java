73afd73a48ef87653436cae1e98a20b6bad1e9a9
aclement <aclement>
2007-10-21 18:24:38 +0000

    delay required for some filesystems that have poor resolution of lastmodtime

--- tests/src/org/aspectj/systemtest/incremental/tools/AbstractMultiProjectIncrementalAjdeInteractionTestbed.java
+++ tests/src/org/aspectj/systemtest/incremental/tools/AbstractMultiProjectIncrementalAjdeInteractionTestbed.java
@@ -123,6 +123,7 @@ public class AbstractMultiProjectIncrementalAjdeInteractionTestbed extends
 	 * copy is recursive.
 	 */
 	protected void copy(File from, File to) {
+		try { Thread.sleep(1100); } catch (Exception e) {}
 		String contents[] = from.list();
 		if (contents==null) return;
 		for (int i = 0; i < contents.length; i++) {
