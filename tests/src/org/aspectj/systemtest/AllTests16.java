35a5fcca4f588a3d0a0ba48f70035b15e9319640
aclement <aclement>
2008-01-16 23:52:43 +0000

    AspectJ6: test suites

--- tests/src/org/aspectj/systemtest/AllTests16.java
+++ tests/src/org/aspectj/systemtest/AllTests16.java
@@ -7,12 +7,14 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.aspectj.systemtest.ajc154.AllTestsAspectJ154;
+import org.aspectj.systemtest.ajc160.AllTestsAspectJ160;
 
 public class AllTests16 {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("AspectJ System Test Suite - JDK 1.6");
 		//$JUnit-BEGIN$
+		suite.addTest(AllTestsAspectJ160.suite()); // dont require a 1.6 JRE to run but checks 1.6 compiler behaviour
 		suite.addTest(AllTests15.suite());
 		//$JUnit-END$
 		return suite;
