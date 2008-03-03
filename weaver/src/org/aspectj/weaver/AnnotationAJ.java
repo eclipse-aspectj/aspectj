6a34940cfc2e8fe9461f9888544df72ae6c4f31b
aclement <aclement>
2008-03-03 17:23:34 +0000

    annotation value matching in decp

--- weaver/src/org/aspectj/weaver/AnnotationAJ.java
+++ weaver/src/org/aspectj/weaver/AnnotationAJ.java
@@ -15,6 +15,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.aspectj.apache.bcel.classfile.annotation.ElementNameValuePair;
+
 /**
  * This type represents the weavers abstraction of an annotation - it is
  * not tied to any underlying BCI toolkit.  The weaver actualy handles these
@@ -94,5 +96,28 @@ public class AnnotationAJ {
 		}
 		sb.append("]");
 		return sb.toString();
+	}
+
+	public boolean hasNamedValue(String n) {
+		if (nvPairs==null) return false;
+		for (int i=0;i<nvPairs.size();i++) {
+			AnnotationNameValuePair pair = (AnnotationNameValuePair)nvPairs.get(i);
+			if (pair.getName().equals(n)) return true;
+		}
+		return false;
+	}
+
+	/**
+     * Return true if the annotation has a value with the specified name (n) and value (v)
+     */
+	public boolean hasNameValuePair(String n, String v) {
+		if (nvPairs==null) return false;
+		for (int i=0;i<nvPairs.size();i++) {
+			AnnotationNameValuePair pair = (AnnotationNameValuePair)nvPairs.get(i);
+			if (pair.getName().equals(n)) {
+				if (pair.getValue().stringify().equals(v)) return true;
+			}
+		}
+		return false;
 	}
 }
