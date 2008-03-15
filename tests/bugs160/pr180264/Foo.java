84b0374d8a6d88141a1ae6cf06183262169d7d8c
aclement <aclement>
2008-03-15 00:05:53 +0000

    180264 - testcode

---
+++ tests/bugs160/pr180264/Foo.java
@@ -0,0 +1,8 @@
+package a;
+
+public class Foo {
+    private static void foo(Object a) {
+    }
+    public static void foo(int[] a) {
+    }  
+}
---
+++ tests/bugs160/pr180264/Main.java
@@ -0,0 +1,9 @@
+package a;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Object a = null;
+        Foo.foo((int[])a);
+    }
+}
