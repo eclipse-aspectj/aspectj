75d917cdcf62a4c81befbfd535e38bb2b80a6466
aclement <aclement>
2008-03-20 00:12:10 +0000

    164356: test and fix: ajdoc

---
+++ tests/bugs153/pr164356/C.java
@@ -0,0 +1,21 @@
+public class C {
+
+    // foo's ordinary comment
+    /**
+     * description of foo
+     */
+    public void foo() {
+    }
+
+    //************
+    /**
+     * description of bar
+     */
+    public void bar() {
+    }
+    
+    //*******
+    public void goo() {
+    }
+
+}
