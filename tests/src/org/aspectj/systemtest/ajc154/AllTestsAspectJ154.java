3e5aa808e98784e6329947fd43640874e449c1d0
aclement <aclement>
2008-05-07 04:38:11 +0000

    removing test that builds into the src tree rather than the sandbox

--- tests/src/org/aspectj/systemtest/ajc154/AllTestsAspectJ154.java
+++ tests/src/org/aspectj/systemtest/ajc154/AllTestsAspectJ154.java
@@ -20,7 +20,8 @@ public class AllTestsAspectJ154 {
 		TestSuite suite = new TestSuite("AspectJ 1.5.4 tests");
 		//$JUnit-BEGIN$
 		suite.addTest(Ajc154Tests.suite());
-		suite.addTestSuite(CustomMungerExtensionTest.class);
+		// removed for now - it keeps creating unwanted directories in the source tree rather than building into the sandbox
+//		suite.addTestSuite(CustomMungerExtensionTest.class);
         //$JUnit-END$
 		return suite;
 	}
