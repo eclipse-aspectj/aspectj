15417326c953d13c906f3e7d516fcf64ed04d36a
aclement <aclement>
2008-06-10 22:46:14 +0000

    230075: testcode and fix

--- tests/src/org/aspectj/systemtest/ajc161/Ajc161Tests.java
+++ tests/src/org/aspectj/systemtest/ajc161/Ajc161Tests.java
@@ -23,6 +23,7 @@ import org.aspectj.testing.XMLBasedAjcTestCase;
 public class Ajc161Tests extends org.aspectj.testing.XMLBasedAjcTestCase {
 	
 	// AspectJ1.6.1
+	public void testProtectedMethodsAroundAdvice_pr230075() { runTest("protected methods and around advice");}
 	public void testFinalStringsAnnotationPointcut_pr174385() { runTest("static strings in annotation pointcuts");}
 	public void testComplexBoundsGenericAspect_pr199130_1() { runTest("complex bounds on generic aspect - 1");}
 	public void testComplexBoundsGenericAspect_pr199130_2() { runTest("complex bounds on generic aspect - 2");}
