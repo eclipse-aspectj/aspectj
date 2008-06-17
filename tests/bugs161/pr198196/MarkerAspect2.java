da5aaae9a023e4bd2bc28a29fa84d626fce9682e
aclement <aclement>
2008-06-17 02:24:10 +0000

    198196: prevent stackoverflow

--- tests/bugs161/pr198196/MarkerAspect2.java
+++ tests/bugs161/pr198196/MarkerAspect2.java
@@ -1,12 +1,15 @@
 aspect MarkerAspect2  { // IncompatibleClassChangeError
 
+		int Marker.i=0;
 
         declare parents: Foo implements Marker;
 
         public String Marker.toString() {
                 new Runnable() {
                         public void run() {
-                                Marker.super.toString();
+                    		if (i++>5) return;
+                    		System.out.println("a");
+                            Marker.super.toString();
                         }
                 }.run();
                 return "banana";//super.toString();
