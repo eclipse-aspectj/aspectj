979d45b4969a65babd729e9253c4ac43536f24ff
aclement <aclement>
2008-06-24 02:00:46 +0000

    180264: uselessTypeChecks and ASTNode: refix

--- tests/src/org/aspectj/systemtest/ajc160/Ajc160Tests.java
+++ tests/src/org/aspectj/systemtest/ajc160/Ajc160Tests.java
@@ -43,9 +43,9 @@ public class Ajc160Tests extends org.aspectj.testing.XMLBasedAjcTestCase {
     // public void testBrokenIfArgsCflowAtAj_pr145018() {
     //    runTest("ataj crashing with cflow, if and args");
     // }
-    // public void testClassCastOnArrayType_pr180264() {
-    // runTest("classcastexception on array type");
-    // }
+    public void testClassCastOnArrayType_pr180264() {
+      runTest("classcastexception on array type");
+    }
     //public void testITDWithArray_pr201748() { runTest("itd with array");}
 	public void testBadMessage() { runTest("incorrect itd error with generics");}
 	public void testBadMessage2() { runTest("incorrect itd error with generics - 2");}
