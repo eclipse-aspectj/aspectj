f3ae74bda5f98e207ba43d7bb63947a6128ff904
aclement <aclement>
2008-08-28 00:04:23 +0000

    chewed by formatter

--- bcel-builder/src/org/aspectj/apache/bcel/generic/LocalVariableGen.java
+++ bcel-builder/src/org/aspectj/apache/bcel/generic/LocalVariableGen.java
@@ -55,152 +55,166 @@ package org.aspectj.apache.bcel.generic;
  */
 
 import org.aspectj.apache.bcel.Constants;
-import org.aspectj.apache.bcel.classfile.LocalVariable;
 import org.aspectj.apache.bcel.classfile.ConstantPool;
+import org.aspectj.apache.bcel.classfile.LocalVariable;
 
-/** 
- * This class represents a local variable within a method. It contains its
- * scope, name and type. The generated LocalVariable object can be obtained
- * with getLocalVariable which needs the instruction list and the constant
- * pool as parameters.
- *
- * @version $Id: LocalVariableGen.java,v 1.6 2008/05/28 23:52:54 aclement Exp $
- * @author  <A HREF="mailto:markus.dahm@berlin.de">M. Dahm</A>
- * @see     LocalVariable
- * @see     MethodGen
+/**
+ * This class represents a local variable within a method. It contains its scope, name and type. The generated LocalVariable object
+ * can be obtained with getLocalVariable which needs the instruction list and the constant pool as parameters.
+ * 
+ * @version $Id: LocalVariableGen.java,v 1.7 2008/08/28 00:04:23 aclement Exp $
+ * @author <A HREF="mailto:markus.dahm@berlin.de">M. Dahm</A>
+ * @see LocalVariable
+ * @see MethodGen
  */
-public class LocalVariableGen
-  implements InstructionTargeter, /*NamedAndTyped,*/ Cloneable,
-	     java.io.Serializable
-{
-  private int         index;
-  private String      name;
-  private Type        type;
-  private InstructionHandle start, end;
+public class LocalVariableGen implements InstructionTargeter, Cloneable, java.io.Serializable {
+	private int index;
+	private String name;
+	private Type type;
+	private InstructionHandle start, end;
 
-  /**
-   * Generate a local variable that with index `index'. Note that double and long
-   * variables need two indexs. Index indices have to be provided by the user.
-   *
-   * @param index index of local variable
-   * @param name its name
-   * @param type its type
-   * @param start from where the instruction is valid (null means from the start)
-   * @param end until where the instruction is valid (null means to the end)
-   */
-  public LocalVariableGen(int index, String name, Type type,
-			  InstructionHandle start, InstructionHandle end) {
-    if((index < 0) || (index > Constants.MAX_SHORT))
-      throw new ClassGenException("Invalid index index: " + index);
-    
-    this.name  = name;
-    this.type  = type;
-    this.index  = index;
-    setStart(start);
-    setEnd(end);
-  }
+	/**
+	 * Generate a local variable that with index `index'. Note that double and long variables need two indexs. Index indices have to
+	 * be provided by the user.
+	 * 
+	 * @param index index of local variable
+	 * @param name its name
+	 * @param type its type
+	 * @param start from where the instruction is valid (null means from the start)
+	 * @param end until where the instruction is valid (null means to the end)
+	 */
+	public LocalVariableGen(int index, String name, Type type, InstructionHandle start, InstructionHandle end) {
+		if (index < 0 || index > Constants.MAX_SHORT) {
+			throw new ClassGenException("Invalid index index: " + index);
+		}
 
-  /**
-   * Get LocalVariable object.
-   *
-   * This relies on that the instruction list has already been dumped to byte code or
-   * or that the `setPositions' methods has been called for the instruction list.
-   *
-   * Note that for local variables whose scope end at the last
-   * instruction of the method's code, the JVM specification is ambiguous:
-   * both a start_pc+length ending at the last instruction and
-   * start_pc+length ending at first index beyond the end of the code are
-   * valid.
-   *
-   * @param il instruction list (byte code) which this variable belongs to
-   * @param cp constant pool
-   */
-  public LocalVariable getLocalVariable(ConstantPool cp) {
-    int start_pc        = start.getPosition();
-    int length          = end.getPosition() - start_pc;
+		this.name = name;
+		this.type = type;
+		this.index = index;
+		setStart(start);
+		setEnd(end);
+	}
 
-    if(length > 0) {
-      length += end.getInstruction().getLength();
-   }
-    
-    int name_index      = cp.addUtf8(name);
-    int signature_index = cp.addUtf8(type.getSignature());
+	/**
+	 * Get LocalVariable object.
+	 * 
+	 * This relies on that the instruction list has already been dumped to byte code or or that the `setPositions' methods has been
+	 * called for the instruction list.
+	 * 
+	 * Note that for local variables whose scope end at the last instruction of the method's code, the JVM specification is
+	 * ambiguous: both a start_pc+length ending at the last instruction and start_pc+length ending at first index beyond the end of
+	 * the code are valid.
+	 * 
+	 * @param il instruction list (byte code) which this variable belongs to
+	 * @param cp constant pool
+	 */
+	public LocalVariable getLocalVariable(ConstantPool cp) {
+		int start_pc = start.getPosition();
+		int length = end.getPosition() - start_pc;
 
-    return new LocalVariable(start_pc, length, name_index,
-			     signature_index, index, cp);
-  }
+		if (length > 0) {
+			length += end.getInstruction().getLength();
+		}
 
-  public void        setIndex(int index)           { this.index = index; }
-  public int         getIndex()                   { return index; }
-  public void        setName(String name)        { this.name = name; }
-  public String      getName()                   { return name; }
-  public void        setType(Type type)          { this.type = type; }
-  public Type        getType()                   { return type; }
+		int name_index = cp.addUtf8(name);
+		int signature_index = cp.addUtf8(type.getSignature());
 
-  public InstructionHandle getStart()                  { return start; }
-  public InstructionHandle getEnd()                    { return end; }
+		return new LocalVariable(start_pc, length, name_index, signature_index, index, cp);
+	}
 
-  public void setStart(InstructionHandle start) {
-	  InstructionBranch.notifyTarget(this.start, start, this);
-    this.start = start;
-  }
+	public void setIndex(int index) {
+		this.index = index;
+	}
 
-  public void setEnd(InstructionHandle end) {
-	  InstructionBranch.notifyTarget(this.end, end, this);
-    this.end = end;
-  }
+	public int getIndex() {
+		return index;
+	}
 
-  /**
-   * @param old_ih old target, either start or end
-   * @param new_ih new target
-   */
-  public void updateTarget(InstructionHandle old_ih, InstructionHandle new_ih) {
-    boolean targeted = false;
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    if(start == old_ih) {
-      targeted = true;
-      setStart(new_ih);
-    }
+	public String getName() {
+		return name;
+	}
 
-    if(end == old_ih) {
-      targeted = true;
-      setEnd(new_ih);
-    }
+	public void setType(Type type) {
+		this.type = type;
+	}
 
-    if(!targeted)
-      throw new ClassGenException("Not targeting " + old_ih + ", but {" + start + ", " +
-				  end + "}");
-  }
+	public Type getType() {
+		return type;
+	}
 
-  /**
-   * @return true, if ih is target of this variable
-   */
-  public boolean containsTarget(InstructionHandle ih) {
-    return (start == ih) || (end == ih);
-  }
+	public InstructionHandle getStart() {
+		return start;
+	}
 
-  /**
-   * We consider to local variables to be equal, if the use the same index and
-   * are valid in the same range.
-   */
-  public boolean equals(Object o) {
-    if(!(o instanceof LocalVariableGen))
-      return false;
+	public InstructionHandle getEnd() {
+		return end;
+	}
 
-    LocalVariableGen l = (LocalVariableGen)o;
-    return (l.index == index) && (l.start == start) && (l.end == end);
-  } 
+	public void setStart(InstructionHandle start) {
+		InstructionBranch.notifyTarget(this.start, start, this);
+		this.start = start;
+	}
 
-  public String toString() {
-    return "LocalVariableGen(" + name +  ", " + type +  ", " + start + ", " + end + ")";
-  }
+	public void setEnd(InstructionHandle end) {
+		InstructionBranch.notifyTarget(this.end, end, this);
+		this.end = end;
+	}
 
-  public Object clone() {
-    try {
-      return super.clone();
-    } catch(CloneNotSupportedException e) {
-      System.err.println(e);
-      return null;
-    }
-  }
+	/**
+	 * @param old_ih old target, either start or end
+	 * @param new_ih new target
+	 */
+	public void updateTarget(InstructionHandle old_ih, InstructionHandle new_ih) {
+		boolean targeted = false;
+
+		if (start == old_ih) {
+			targeted = true;
+			setStart(new_ih);
+		}
+
+		if (end == old_ih) {
+			targeted = true;
+			setEnd(new_ih);
+		}
+
+		if (!targeted) {
+			throw new ClassGenException("Not targeting " + old_ih + ", but {" + start + ", " + end + "}");
+		}
+	}
+
+	/**
+	 * @return true, if ih is target of this variable
+	 */
+	public boolean containsTarget(InstructionHandle ih) {
+		return start == ih || end == ih;
+	}
+
+	/**
+	 * We consider to local variables to be equal, if the use the same index and are valid in the same range.
+	 */
+	public boolean equals(Object o) {
+		if (!(o instanceof LocalVariableGen)) {
+			return false;
+		}
+
+		LocalVariableGen l = (LocalVariableGen) o;
+		return l.index == index && l.start == start && l.end == end;
+	}
+
+	public String toString() {
+		return "LocalVariableGen(" + name + ", " + type + ", " + start + ", " + end + ")";
+	}
+
+	public Object clone() {
+		try {
+			return super.clone();
+		} catch (CloneNotSupportedException e) {
+			System.err.println(e);
+			return null;
+		}
+	}
 }
