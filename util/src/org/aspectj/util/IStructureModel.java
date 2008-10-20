77a2ad53f201fe3947b94edd6c7b8d4d2a5ed88a
aclement <aclement>
2008-10-20 23:34:31 +0000

    246125: new abstraction of structure model without the weight of the asm module

---
+++ util/src/org/aspectj/util/IStructureModel.java
@@ -0,0 +1,21 @@
+/* *******************************************************************
+ * Copyright (c) 2008 Contributors.
+ * All rights reserved. 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution and is available at 
+ * http://eclipse.org/legal/epl-v10.html 
+ *  
+ * Contributors: 
+ *   Andy Clement
+ * ******************************************************************/
+package org.aspectj.util;
+
+/**
+ * Abstraction of a structure model
+ * 
+ * @author Andy Clement
+ */
+public interface IStructureModel {
+
+}
