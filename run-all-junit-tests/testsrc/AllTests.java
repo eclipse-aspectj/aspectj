06607800a13d0f03a169ed4e9c36838c7bfd876e
aclement <aclement>
2008-10-28 20:34:07 +0000

    246125: pulled tests from weaver into matcher and created bcel subtypes of matcher tests where appropriate

--- run-all-junit-tests/testsrc/AllTests.java
+++ run-all-junit-tests/testsrc/AllTests.java
@@ -35,45 +35,47 @@ import org.aspectj.tools.ant.TaskdefsModuleTests;
 import org.aspectj.util.LangUtil;
 import org.aspectj.util.UtilModuleTests;
 import org.aspectj.weaver.BcweaverModuleTests;
+import org.aspectj.weaver.MatcherModuleTests;
 import org.aspectj.weaver.Weaver5ModuleTests;
 
 public class AllTests extends TestCase {
-    public static final boolean skipSupportModules = false;
-    
-    public static TestSuite suite() {
-        TestSuite suite = new TestSuite(AllTests.class.getName());
-        suite.addTest(AjbrowserModuleTests.suite());
-        suite.addTest(AjdeModuleTests.suite());
-        suite.addTest(AjdeCoreModuleTests.suite());
-        suite.addTest(AjdocModuleTests.suite());
-        suite.addTest(AsmModuleTests.suite());
-        suite.addTest(BridgeModuleTests.suite());
-        suite.addTest(LoadtimeModuleTests.suite());
-        suite.addTest(EajcModuleTests.suite());
-        //suite.addTest(LibModuleTests.suite());
-        suite.addTest(RuntimeModuleTests.suite());
-        suite.addTest(TaskdefsModuleTests.suite());
-        if (!skipSupportModules) {
-            suite.addTest(BuildModuleTests.suite());
-            suite.addTest(TestingModuleTests.suite());
-            suite.addTest(TestingClientModuleTests.suite());
-            suite.addTest(TestingDriversModuleTests.suite());
-            suite.addTest(TestingUtilModuleTests.suite());
-        }
-        suite.addTest(UtilModuleTests.suite());
-        suite.addTest(BcweaverModuleTests.suite());
-        if (LangUtil.is15VMOrGreater()) {
-            suite.addTest(Aspectj5rtModuleTests.suite());
-            suite.addTest(Loadtime5ModuleTests.suite());
-            suite.addTest(Weaver5ModuleTests.suite());
-        } else {
-            suite.addTest(TestUtil.skipTest("for 1.5"));
-        } 
-        return suite;
-    }
+	public static final boolean skipSupportModules = false;
 
-    public AllTests(String name) {
-        super(name);
-    }
+	public static TestSuite suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		suite.addTest(AjbrowserModuleTests.suite());
+		suite.addTest(AjdeModuleTests.suite());
+		suite.addTest(AjdeCoreModuleTests.suite());
+		suite.addTest(AjdocModuleTests.suite());
+		suite.addTest(AsmModuleTests.suite());
+		suite.addTest(BridgeModuleTests.suite());
+		suite.addTest(LoadtimeModuleTests.suite());
+		suite.addTest(EajcModuleTests.suite());
+		// suite.addTest(LibModuleTests.suite());
+		suite.addTest(RuntimeModuleTests.suite());
+		suite.addTest(TaskdefsModuleTests.suite());
+		if (!skipSupportModules) {
+			suite.addTest(BuildModuleTests.suite());
+			suite.addTest(TestingModuleTests.suite());
+			suite.addTest(TestingClientModuleTests.suite());
+			suite.addTest(TestingDriversModuleTests.suite());
+			suite.addTest(TestingUtilModuleTests.suite());
+		}
+		suite.addTest(UtilModuleTests.suite());
+		suite.addTest(MatcherModuleTests.suite());
+		suite.addTest(BcweaverModuleTests.suite());
+		if (LangUtil.is15VMOrGreater()) {
+			suite.addTest(Aspectj5rtModuleTests.suite());
+			suite.addTest(Loadtime5ModuleTests.suite());
+			suite.addTest(Weaver5ModuleTests.suite());
+		} else {
+			suite.addTest(TestUtil.skipTest("for 1.5"));
+		}
+		return suite;
+	}
+
+	public AllTests(String name) {
+		super(name);
+	}
 
 }
