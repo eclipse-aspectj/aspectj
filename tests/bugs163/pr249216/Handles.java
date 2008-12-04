0daedad7d44b9233d5c1f5a726d2a95d7c54cc21
aclement <aclement>
2008-12-04 02:04:02 +0000

    249216: more test cases

--- tests/bugs163/pr249216/Handles.java
+++ tests/bugs163/pr249216/Handles.java
@@ -7,6 +7,26 @@ public aspect Handles {
   public java.util.List<String> Ship.i(Set<String>[][] u) {
 	        return null;
   }
+  public java.util.Set<String> i(java.util.Set<String>[][] u) {
+                return null;
+  }
+  public java.util.Set<String> i(java.util.Set<String>[][] u,int i) {
+                return null;
+  }
+  public java.util.Set<String> i2(java.util.Set<? extends Collection<String>>[][] u) {
+    return null;
+  }
+  public java.util.Set<String> i3(java.util.Set<? extends Collection<String[]>>[][] u) {
+    return null;
+  }
+  public java.util.Set<String> i4(java.util.Set<? extends Collection<String>> u) {
+    return null;
+  }
+  public java.util.Set<String> i5(java.util.Set<?> u) {
+    return null;
+  }
+  
+  
 }
 
 class Ship {}
