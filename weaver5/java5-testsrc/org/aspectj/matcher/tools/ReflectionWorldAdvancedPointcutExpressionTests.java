e58cc15a92634c2cdd1913a8f670bd03f9eb67fe
aclement <aclement>
2008-12-11 00:17:26 +0000

    missing license + removed from suite

--- weaver5/java5-testsrc/org/aspectj/matcher/tools/ReflectionWorldAdvancedPointcutExpressionTests.java
+++ weaver5/java5-testsrc/org/aspectj/matcher/tools/ReflectionWorldAdvancedPointcutExpressionTests.java
@@ -1,6 +1,16 @@
+/* *******************************************************************
+ * Copyright (c) 2008 Contributors
+ * All rights reserved. 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution and is available at 
+ * http://eclipse.org/legal/epl-v10.html 
+ *  
+ * Contributors: 
+ * Andy Clement
+ * ******************************************************************/
 package org.aspectj.matcher.tools;
 
-import org.aspectj.matcher.tools.CommonAdvancedPointcutExpressionTests;
 import org.aspectj.weaver.World;
 import org.aspectj.weaver.reflect.ReflectionWorld;
 
