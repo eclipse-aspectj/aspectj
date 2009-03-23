525a70ecf3e81ac0bb3c5a48c6ea1850ee2c0b59
aclement <aclement>
2009-03-23 02:29:56 +0000

    generalize report API to cover resources and outjar

--- ajde.core/testsrc/org/aspectj/ajde/core/TestOutputLocationManager.java
+++ ajde.core/testsrc/org/aspectj/ajde/core/TestOutputLocationManager.java
@@ -79,10 +79,10 @@ public class TestOutputLocationManager implements IOutputLocationManager {
 		return null;
 	}
 
-	public void reportClassFileWrite(String outputfile) {
+	public void reportFileWrite(String outputfile, int filetype) {
 	}
 
-	public void reportClassFileRemove(String outputfile) {
+	public void reportFileRemove(String outputfile, int filetype) {
 	}
 
 }
