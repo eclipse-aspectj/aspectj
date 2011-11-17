74e9c7af2b24e2d658f4677a1d8d7f02e9b0a087
aclement <aclement>
2011-11-17 18:16:37 +0000

    moved to 1.7

--- tests/testsrc/org/aspectj/tests/TestsModuleTests.java
+++ tests/testsrc/org/aspectj/tests/TestsModuleTests.java
@@ -1,4 +1,5 @@
 package org.aspectj.tests;
+
 /* *******************************************************************
  * Copyright (c) 2005 Contributors.
  * All rights reserved. 
@@ -11,33 +12,34 @@ package org.aspectj.tests;
  *     Wes Isberg       initial implementation 
  * ******************************************************************/
 
-import junit.framework.TestCase;
 import junit.framework.Test;
+import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import org.aspectj.systemtest.AllTests;
 import org.aspectj.systemtest.AllTests14;
-import org.aspectj.systemtest.AllTests16;
+import org.aspectj.systemtest.AllTests17;
 import org.aspectj.util.LangUtil;
 
 public class TestsModuleTests extends TestCase {
 
-    public static Test suite() {
-        String name = TestsModuleTests.class.getName();
-        TestSuite suite = new TestSuite(name);
-        // compiler tests, wrapped for JUnit
-        if (LangUtil.is15VMOrGreater()) {
-//            suite.addTest(AllTests15.suite());
-            suite.addTest(AllTests16.suite()); // there are currently (28/11/06) no tests specific to a 1.6 vm - so we can do this
-        } else if (LangUtil.is14VMOrGreater()) {
-            System.err.println("Skipping tests for 1.5");
-            //suite.addTest(TestUtil.skipTest("for 1.5"));
-            suite.addTest(AllTests14.suite());            
-        } else {
-            System.err.println("Skipping tests for 1.4 and 1.5");
-            //suite.addTest(TestUtil.skipTest("for 1.4 and 1.5"));
-	        suite.addTest(AllTests.suite());            
-        }
-        return suite;
-    }
+	public static Test suite() {
+		String name = TestsModuleTests.class.getName();
+		TestSuite suite = new TestSuite(name);
+		// compiler tests, wrapped for JUnit
+		if (LangUtil.is15VMOrGreater()) {
+			// suite.addTest(AllTests15.suite());
+			suite.addTest(AllTests17.suite()); // there are currently (28/11/06) no tests specific to a 1.6/1.7 vm - so we can do
+												// this
+		} else if (LangUtil.is14VMOrGreater()) {
+			System.err.println("Skipping tests for 1.5");
+			// suite.addTest(TestUtil.skipTest("for 1.5"));
+			suite.addTest(AllTests14.suite());
+		} else {
+			System.err.println("Skipping tests for 1.4 and 1.5");
+			// suite.addTest(TestUtil.skipTest("for 1.4 and 1.5"));
+			suite.addTest(AllTests.suite());
+		}
+		return suite;
+	}
 }
