821f7d4c71c1025dae6990a65fb9549a8530b1ac
Andy Clement <aclement@pivotal.io>
2016-12-02 10:37:28 -0800

    added 2016/2017 to years

--- build/src/org/aspectj/internal/tools/ant/taskdefs/Checklics.java
+++ build/src/org/aspectj/internal/tools/ant/taskdefs/Checklics.java
@@ -338,7 +338,8 @@ public class Checklics extends MatchingTask {
 	public static class License {
 		/** acceptable years for copyright prefix to company - append " " */
 		static final String[] YEARS = // remove older after license xfer?
-		new String[] { "2002 ", "2003 ", "2004 ", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2001 ", "2000 ",
+		new String[] { "2002 ", "2003 ", "2004 ", "2005", "2006", "2007", "2008", 
+				"2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2001 ", "2000 ",
 				"1999 " };
 		public final String tag;
 		public final String license;
