4d81108f378e12fa516acd6da3b8c1bf5c609c7b
Andy Clement <aclement@pivotal.io>
2018-02-14 12:40:22 -0800

    Add intermediate test class that ensures the tests are on J9

---
+++ testing/newsrc/org/aspectj/testing/XMLBasedAjcTestCaseForJava9OrLater.java
@@ -0,0 +1,31 @@
+/* *******************************************************************
+ * Copyright (c) 2018 Contributors
+ * All rights reserved. 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ * Contributors: 
+ *     Andy Clement
+ * ******************************************************************/
+package org.aspectj.testing;
+
+/**
+ * Makes sure tests are running on the right level of JDK.
+ * 
+ * @author Andy Clement
+ */
+public abstract class XMLBasedAjcTestCaseForJava9OrLater extends XMLBasedAjcTestCase {
+
+	@Override
+	public void runTest(String title) {
+		// Check we are on Java9
+		String property = System.getProperty("java.version");
+		if (!property.startsWith("9")) {
+			throw new IllegalStateException("These tests should be run on Java 9 or later");
+		}
+		super.runTest(title);
+	}
+	
+}
