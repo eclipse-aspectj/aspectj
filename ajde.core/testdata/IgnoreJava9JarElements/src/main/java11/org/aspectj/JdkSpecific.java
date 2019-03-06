7b81556cebccfacfc94ca00f5af88987c87cdefb
Andy Clement <aclement@pivotal.io>
2019-03-06 07:40:20 -0800

    Fix AspectJ 1.8.x fails with modular and multi-release JARs

---
+++ ajde.core/testdata/IgnoreJava9JarElements/src/main/java11/org/aspectj/JdkSpecific.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.aspectj;
+
+public class JdkSpecific {
+     public static void main(String... args) {
+         System.out.println("This is the Java 11 version of the class.");
+     }
+}
