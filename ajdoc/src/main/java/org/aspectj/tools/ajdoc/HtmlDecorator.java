798ee65035142594c43c8c154baaafd557d5d11e
Alexander Kriegisch <Alexander@Kriegisch.name>
2021-03-14 23:41:11 +0700

    Use static import for HtmlDecorator.TYPE_NAME_LABEL
    Mini refactoring, making the code a bit less verbose
    
    Signed-off-by: Alexander Kriegisch <Alexander@Kriegisch.name>

--- ajdoc/src/main/java/org/aspectj/tools/ajdoc/HtmlDecorator.java
+++ ajdoc/src/main/java/org/aspectj/tools/ajdoc/HtmlDecorator.java
@@ -275,7 +275,7 @@ class HtmlDecorator {
 					// Java15: <pre>static class <span class="type-name-label">ClassA.InnerAspect</span>
 					// Java8: <pre>static class <span class="typeNameLabel">ClassA.InnerAspect</span>
 					// Java7 (464604): <pre>public class <span class="strong">Azpect</span>
-					classStartIndex = fileContents.toString().indexOf("class <span class=\"" + HtmlDecorator.TYPE_NAME_LABEL + "\">");
+					classStartIndex = fileContents.toString().indexOf("class <span class=\"" + TYPE_NAME_LABEL + "\">");
 					int classEndIndex = fileContents.toString().indexOf("</span>", classStartIndex);
 					if (classEndIndex != -1) {
 						// Convert it to "aspect <span class="TYPE_NAME_LABEL">ClassA.InnerAspect</span>"
