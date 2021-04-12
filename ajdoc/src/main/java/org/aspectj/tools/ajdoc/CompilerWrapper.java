f00f547d7b37647880e7f8fc030f9f96dbad9848
Alexander Kriegisch <Alexander@Kriegisch.name>
2021-04-12 13:16:29 +0700

    Clean up Maven dependencies using 'dependency:analyze' goal
    Notably, this change involves a partial revert of @4a5660b3, because we
    are not using JUnit Jupiter yet but still JUnit 4 tests. See discussion
    under commit at https://github.com/eclipse/org.aspectj/commit/4a5660b3.
    
    Many other warnings - concerning both used undeclared and unused
    declared dependencies - were eliminated by adding or removing the
    corresponding dependencies from the POMs. Furthermore, I tried to make
    sure that some clearly test-scoped dependencies are now actually
    declared as such, so as to avoid unwanted transitivity bleeding into
    compile scope and maybe unwanted classes ending up in uber JARs via
    Maven Shade or Maven Assembly.
    
    TODO: I am not so sure why modules other than 'run-all-unit-tests' would
    depend on test JARs. I hope I broke nothing essential there. As of
    today, the other modules where I found '<type>test-jar</type>'
    dependencies are:
      - ajde
      - testing
      - testing-drivers
      - tests
      - weaver
    
    Signed-off-by: Alexander Kriegisch <Alexander@Kriegisch.name>

--- ajdoc/src/main/java/org/aspectj/tools/ajdoc/CompilerWrapper.java
+++ ajdoc/src/main/java/org/aspectj/tools/ajdoc/CompilerWrapper.java
@@ -1,25 +1,25 @@
 /* *******************************************************************
  * Copyright (c) 2003 Contributors.
- * All rights reserved. 
- * This program and the accompanying materials are made available 
- * under the terms of the Eclipse Public License v1.0 
- * which accompanies this distribution and is available at 
- * http://www.eclipse.org/legal/epl-v10.html 
- *  
- * Contributors: 
- *     Mik Kersten     initial implementation 
+ * All rights reserved.
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Mik Kersten     initial implementation
  * ******************************************************************/
 package org.aspectj.tools.ajdoc;
 
 import org.aspectj.asm.AsmManager;
 import org.aspectj.bridge.IMessage;
-
+import org.aspectj.tools.ajc.Main;
 /**
  * Wrapper for ajdoc's use of the AspectJ compiler.
- * 
+ *
  * @author Mik Kersten
  */
-public class CompilerWrapper extends org.aspectj.tools.ajc.Main {
+public class CompilerWrapper extends Main {
 
 	private static CompilerWrapper INSTANCE = null;
 
