5eb286d3c281a711d0bfd6effad7bdbc03f6f61c
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-03-31 15:21:13 +0700

    UseJDKExtendedCharsets.java: add "how to fix test" comment
    Signed-off-by: Alexander Kriegisch <Alexander@Kriegisch.name>

--- tests/bugs199/github_145/add_modules/UseJDKExtendedCharsets.java
+++ tests/bugs199/github_145/add_modules/UseJDKExtendedCharsets.java
@@ -1,6 +1,13 @@
 import java.nio.charset.Charset;
 import sun.nio.cs.ext.ExtendedCharsets;
 
+/**
+ * Note that the Windows JDK knows many more extended charsets than the Linux one. Originally, this test was using
+ * "hebrew", but that yielded failing GitHub CI tests due to the unavailability of the character set there. I actually
+ * had to inspect a Linux 'lib/modules' file using 'jimage' in order to filter for available 'ISO*' classes in the
+ * extended character set package. So if this test ever breaks again on any OS platform, you know where to look for the
+ * root cause and have a clue how to fix the test.
+ */
 public class UseJDKExtendedCharsets {
   static ExtendedCharsets charsets = new ExtendedCharsets();
   static Charset iso2022jp = charsets.charsetForName("ISO-2022-JP");
