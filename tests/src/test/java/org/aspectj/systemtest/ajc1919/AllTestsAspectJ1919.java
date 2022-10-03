ec67725ea41ae69453d4ee2624b311746aab3c26
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-10-03 17:17:50 +0200

    Add the first few Java 19 tests
    For now, only the "pattern matching for switch" tests from previews 1
    and 2 were adjusted to work in preview 3, because guarded patterns were
    replaced by 'when' clauses in 'switch' blocks. Therefore, existing test
    classes did not compile anymore and had to be replaced by syntactically
    upgraded versions with content merged from preview 1 and 2 classes.
    
    Signed-off-by: Alexander Kriegisch <Alexander@Kriegisch.name>

---
+++ tests/src/test/java/org/aspectj/systemtest/ajc1919/AllTestsAspectJ1919.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.txt
+ *******************************************************************************/
+package org.aspectj.systemtest.ajc1919;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.aspectj.util.LangUtil;
+
+/**
+ * @author Alexander Kriegisch
+ */
+public class AllTestsAspectJ1919 {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("AspectJ 1.9.19 tests");
+		suite.addTest(Bugs1919Tests.suite());
+		if (LangUtil.is19VMOrGreater()) {
+			suite.addTest(SanityTestsJava19.suite());
+			suite.addTest(Ajc1919TestsJava.suite());
+		}
+		// Do not run tests using a previous compiler's preview features anymore. They would all fail.
+		// TODO: Comment out the following block when upgrading JDT Core to Java 20
+		if (LangUtil.is19VMOrGreater() && !LangUtil.is20VMOrGreater()) {
+			suite.addTest(Java19PreviewFeaturesTests.suite());
+		}
+		return suite;
+	}
+}
---
+++ tests/src/test/java/org/aspectj/systemtest/ajc1919/Java19PreviewFeaturesTests.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.txt
+ *******************************************************************************/
+package org.aspectj.systemtest.ajc1919;
+
+import junit.framework.Test;
+import org.aspectj.apache.bcel.Constants;
+import org.aspectj.testing.XMLBasedAjcTestCase;
+import org.aspectj.testing.XMLBasedAjcTestCaseForJava19Only;
+
+/**
+ * @author Alexander Kriegisch
+ */
+public class Java19PreviewFeaturesTests extends XMLBasedAjcTestCaseForJava19Only {
+
+  public void testSwitchPatternMatchingPreview3Java() {
+    runTest("switch pattern matching preview 3 java");
+    checkVersion("SwitchPatternPreview3OK", Constants.MAJOR_19, Constants.PREVIEW_MINOR_VERSION);
+  }
+
+  public void testSwitchPatternMatchingPreview3Aspect() {
+    runTest("switch pattern matching preview 3 aspect");
+    checkVersion("SwitchPatternPreview3Aspect", Constants.MAJOR_19, Constants.PREVIEW_MINOR_VERSION);
+    checkVersion("Application", Constants.MAJOR_19, Constants.PREVIEW_MINOR_VERSION);
+    checkVersion("Shape", Constants.MAJOR_19, Constants.PREVIEW_MINOR_VERSION);
+    checkVersion("S", Constants.MAJOR_19, Constants.PREVIEW_MINOR_VERSION);
+  }
+
+  public void testSwitchPatternMatchingCaseLabelDominatedByPrecedingError() {
+    runTest("switch pattern matching error");
+  }
+
+  public void testSwitchPatternMatchingPreview3Error1() {
+    runTest("switch pattern matching preview 3 error 1");
+  }
+
+  public void testSwitchPatternMatchingPreview3Error2() {
+    runTest("switch pattern matching preview 3 error 2");
+  }
+
+  public static Test suite() {
+    return XMLBasedAjcTestCase.loadSuite(Java19PreviewFeaturesTests.class);
+  }
+
+  @Override
+  protected java.net.URL getSpecFile() {
+    return getClassResource("ajc1919.xml");
+  }
+
+}
