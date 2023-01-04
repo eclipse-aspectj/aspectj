5f46d44017d020822cf145ce0ff23cc44ecfdfad
Alexander Kriegisch <Alexander@Kriegisch.name>
2023-01-04 14:55:42 +0100

    Fix parenthesised AJ keyword compiler problem
    Fixes #20. Needs JDT Core grammar fix.
    
    Signed-off-by: Alexander Kriegisch <Alexander@Kriegisch.name>

---
+++ tests/bugs1919/github_20/ParenthesisedAJKeywords.java
@@ -0,0 +1,33 @@
+/**
+ * https://github.com/eclipse/org.aspectj/issues/20
+ */
+public class ParenthesisedAJKeywords {
+  public static void main(String[] args) {
+    boolean before = true;
+    int after = 11;
+    String around = "around";
+    boolean aspect = true;
+    int pointcut = 22;
+    String declare = "declare";
+    String privileged = "privileged";
+
+    if ((before)) {
+      System.out.println(foo((before)));
+      switch ((after)) {
+        default: System.out.println("after");
+      }
+      System.out.println((around));
+      System.out.println(!(aspect) ? "no aspect" : "aspect");
+      switch ((pointcut)) {
+        case 22: System.out.println("pointcut"); break;
+        default: System.out.println("xxx");
+      }
+      System.out.println((declare));
+      System.out.println((privileged));
+    }
+  }
+
+  public static String foo(boolean before) {
+    return (before) ? "before" : "after";
+  }
+}
