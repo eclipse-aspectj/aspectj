e0a1525ebd9f0f3b806c323223d4776c1275e899
Alexander Kriegisch <Alexander@Kriegisch.name>
2023-01-06 01:16:31 +0100

    Test cosmetics
    Signed-off-by: Alexander Kriegisch <Alexander@Kriegisch.name>

--- tests/src/test/java/org/aspectj/systemtest/ajc1919/Bugs1919Tests.java
+++ tests/src/test/java/org/aspectj/systemtest/ajc1919/Bugs1919Tests.java
@@ -19,11 +19,11 @@ public class Bugs1919Tests extends XMLBasedAjcTestCase {
     runTest("declare annotation with SOURCE retention");
   }
 
-  public void testsSwitchWith_Integer_MAX_VALUE() {
+  public void testSwitchWith_Integer_MAX_VALUE() {
     runTest("switch with Integer.MAX_VALUE case");
   }
 
-  public void testsParenthesisedExpressionWithAjKeyword() {
+  public void testParenthesisedExpressionWithAjKeyword() {
     runTest("parenthesised expression with AspectJ keyword");
   }
 
