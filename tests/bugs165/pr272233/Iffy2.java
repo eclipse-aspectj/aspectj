667c0bc33c1dc0e5a3612fbaaf71ea55aec975b7
Alexander Kriegisch <Alexander@Kriegisch.name>
2023-01-15 14:16:04 +0100

    Fix failing tests after previous commit
    After the changes, the weaver's behaviour is now slightly different, but
    not actually buggy. Actually, in one case there is now a weaver warning
    for a non-matching pointcut which should have been there before, but was
    not. I.e., things have improved and the tests are even a bit better now.
    
    Signed-off-by: Alexander Kriegisch <Alexander@Kriegisch.name>

--- tests/bugs165/pr272233/Iffy2.java
+++ tests/bugs165/pr272233/Iffy2.java
@@ -1,14 +1,23 @@
 import java.util.*;
 import org.aspectj.lang.annotation.*;
 
-@Aspect 
+@Aspect
 class Iffy2 {
 
-  @Before("execution(!void[] *(..))") 
+  @Before("execution(!void *(..))")
   public void advice1() {}
 
-  @Before("execution(!void[] *(..))") 
+  @Before("execution(!void[] *(..))")
   public void advice2() {}
+
+  @Before("execution(!void *(..))")
+  public void advice3() {}
+
+  @Before("execution(*..Collection[] *(..))")
+  public void advice4() {}
+
+  @Before("execution(java.util.Collection<?>[] *(..))")
+  public void advice5() {}
 
   public Collection<?>[] getCollectionArray() {
         return null;
