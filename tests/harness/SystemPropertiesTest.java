a920edd998df600cfeff615ea5ce067ca8f4bb46
Alexander Kriegisch <Alexander@Kriegisch.name>
2023-03-30 12:03:59 +0200

    Improve SystemPropertiesTest
    - Fix quotation error in console output
    - Throw RuntimeException instead of Error. Maybe this helps to avoid
      the prominent error message listing in the GitHub build log, which
      always looks like something bad happened, even though it is simply an
      expected error.
    
    Signed-off-by: Alexander Kriegisch <Alexander@Kriegisch.name>

--- tests/harness/SystemPropertiesTest.java
+++ tests/harness/SystemPropertiesTest.java
@@ -1,15 +1,13 @@
 
 public class SystemPropertiesTest {
-    public static void main(String[] args) {        
+    public static void main(String[] args) {
         boolean pass = Boolean.getBoolean("PASS");
         if (!pass) {
-            throw new Error("failed to get Boolean \\\"PASS\\\"");
+            throw new RuntimeException("failed to get Boolean \"PASS\"");
         }
         String value = System.getProperty("name", null);
         if (!"value".equals(value)) {
-            throw new Error("failed to get name=\"value\": " + value);
+            throw new RuntimeException("failed to get name=\"value\": " + value);
         }
     }
 }
-
-
