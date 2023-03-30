944010edcf45615d0427e77b3afbff92272148dd
Alexander Kriegisch <Alexander@Kriegisch.name>
2023-03-30 09:13:05 +0200

    eclipse-jdt/eclipse.jdt.core#919 has been fixed
    Signed-off-by: Alexander Kriegisch <Alexander@Kriegisch.name>

--- tests/features1920/java20/SwitchPatternPreview4Error.java
+++ tests/features1920/java20/SwitchPatternPreview4Error.java
@@ -1,11 +1,6 @@
 /**
  * This used to work up to preview 3 (Java 19), but no longer in preview 4 (Java 20).
  * Now, the default case seems to need to be the last one in the list.
- *
- * Javac says 4x: "this case label is dominated by a preceding case label"
- *
- * ECJ/AJC says 11x: "This case label is dominated by one of the preceding case label" (yes, "label" singular)
- * See https://github.com/eclipse-jdt/eclipse.jdt.core/issues/919.
  */
 public class SwitchPatternPreview4Error {
   static void defaultCanAppearBeforePattern(Integer i) {
