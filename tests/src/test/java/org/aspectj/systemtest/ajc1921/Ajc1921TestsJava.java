72171c3724100618bf2c733316d23ff7578eeaa1
Alexander Kriegisch <Alexander@Kriegisch.name>
2023-10-05 11:29:04 +0700

    Make all existing tests run on JDK 21
    This mostly affects pattern matching for switch and record patterns.
    
    Two tests which were working before for pattern matching for switch
    (preview 4), started to fail, so the corresponding code was commented
    out and https://github.com/eclipse-jdt/eclipse.jdt.core/issues/1466
    recorded.
    
    Signed-off-by: Alexander Kriegisch <Alexander@Kriegisch.name>

--- tests/src/test/java/org/aspectj/systemtest/ajc1921/Ajc1921TestsJava.java
+++ tests/src/test/java/org/aspectj/systemtest/ajc1921/Ajc1921TestsJava.java
@@ -8,6 +8,7 @@
 package org.aspectj.systemtest.ajc1921;
 
 import junit.framework.Test;
+import org.aspectj.apache.bcel.Constants;
 import org.aspectj.testing.XMLBasedAjcTestCase;
 import org.aspectj.testing.XMLBasedAjcTestCaseForJava21OrLater;
 
@@ -16,8 +17,78 @@ import org.aspectj.testing.XMLBasedAjcTestCaseForJava21OrLater;
  */
 public class Ajc1921TestsJava extends XMLBasedAjcTestCaseForJava21OrLater {
 
-  public void testDummyJava21() {
-    //runTest("dummy Java 21");
+  public void testSwitchPatternMatchingPreview4Java() {
+    runTest("switch pattern matching preview 4 java");
+    checkVersion("SwitchPatternPreview4OK", Constants.MAJOR_21, Constants.MINOR_21);
+  }
+
+  public void testSwitchPatternMatchingPreview4Error() {
+    runTest("switch pattern matching preview 4 error");
+  }
+
+  public void testSwitchPatternMatchingPreview3Aspect() {
+    runTest("switch pattern matching preview 3 aspect");
+    checkVersion("SwitchPatternPreview3Aspect", Constants.MAJOR_21, Constants.MINOR_21);
+    checkVersion("Application", Constants.MAJOR_21, Constants.MINOR_21);
+    checkVersion("Shape", Constants.MAJOR_21, Constants.MINOR_21);
+    checkVersion("S", Constants.MAJOR_21, Constants.MINOR_21);
+  }
+
+  public void testSwitchPatternMatchingCaseLabelDominatedByPrecedingError() {
+    runTest("switch pattern matching error");
+  }
+
+  public void testSwitchPatternMatchingPreview3Error1() {
+    runTest("switch pattern matching preview 3 error 1");
+  }
+
+  public void testSwitchPatternMatchingPreview3Error2() {
+    runTest("switch pattern matching preview 3 error 2");
+  }
+
+  public void testRecordPatternsPreview1OK() {
+    // https://github.com/eclipse-jdt/eclipse.jdt.core/issues/450
+    runTest("record patterns");
+  }
+
+  public void testRecordPatternsPreview1Error() {
+    // https://github.com/eclipse-jdt/eclipse.jdt.core/issues/450 (fixed for preview 2 in Eclipse 2023-03, 4.27)
+    runTest("record patterns error");
+    checkVersion("RecordPatternsPreview1Error", Constants.MAJOR_21, Constants.MINOR_21);
+    checkVersion("Box", Constants.MAJOR_21, Constants.MINOR_21);
+  }
+
+  public void testRecordPatternsPreview1ExhaustivenessOK1() {
+    // Falsely throws 'An enhanced switch statement should be exhaustive; a default label expected' twice,
+    // see https://github.com/eclipse-jdt/eclipse.jdt.core/issues/455
+    // TODO: Remove redundant default clauses when fixed upstream
+    System.out.println("TODO: fully activate when https://github.com/eclipse-jdt/eclipse.jdt.core/issues/455 has been fixed");
+    runTest("record patterns exhaustiveness 1");
+  }
+
+  public void testRecordPatternsPreview1Aspect() {
+    runTest("record patterns aspect");
+  }
+
+  public void testRecordPatternsPreview1ExhaustivenessAspect() {
+    // Falsely throws 'An enhanced switch statement should be exhaustive; a default label expected' twice,
+    // see https://github.com/eclipse-jdt/eclipse.jdt.core/issues/455
+    // TODO: Remove redundant default clauses when fixed upstream
+    System.out.println("TODO: fully activate when https://github.com/eclipse-jdt/eclipse.jdt.core/issues/455 has been fixed");
+    runTest("record patterns exhaustiveness aspect");
+  }
+
+  public void testRecordPatternsPreview1ExhaustivenessError() {
+    // See https://github.com/eclipse-jdt/eclipse.jdt.core/issues/455
+    runTest("record patterns exhaustiveness error");
+  }
+
+  public void testRecordPatternsPreview1ExhaustivenessOK2() {
+    // Falsely throws 'An enhanced switch statement should be exhaustive; a default label expected',
+    // see https://github.com/eclipse-jdt/eclipse.jdt.core/issues/398
+    // TODO: activate when fixed
+    System.out.println("TODO: activate when https://github.com/eclipse-jdt/eclipse.jdt.core/issues/398 has been fixed");
+    //runTest("record patterns exhaustiveness 2");
   }
 
   public static Test suite() {
--- tests/src/test/java/org/aspectj/systemtest/ajc1921/Java21PreviewFeaturesTests.java
+++ tests/src/test/java/org/aspectj/systemtest/ajc1921/Java21PreviewFeaturesTests.java
@@ -8,7 +8,6 @@
 package org.aspectj.systemtest.ajc1921;
 
 import junit.framework.Test;
-import org.aspectj.apache.bcel.Constants;
 import org.aspectj.testing.XMLBasedAjcTestCase;
 import org.aspectj.testing.XMLBasedAjcTestCaseForJava21Only;
 
@@ -17,78 +16,8 @@ import org.aspectj.testing.XMLBasedAjcTestCaseForJava21Only;
  */
 public class Java21PreviewFeaturesTests extends XMLBasedAjcTestCaseForJava21Only {
 
-  public void testSwitchPatternMatchingPreview4Java() {
-    runTest("switch pattern matching preview 4 java");
-    checkVersion("SwitchPatternPreview4OK", Constants.MAJOR_21, Constants.PREVIEW_MINOR_VERSION);
-  }
-
-  public void testSwitchPatternMatchingPreview4Error() {
-    runTest("switch pattern matching preview 4 error");
-  }
-
-  public void testSwitchPatternMatchingPreview3Aspect() {
-    runTest("switch pattern matching preview 3 aspect");
-    checkVersion("SwitchPatternPreview3Aspect", Constants.MAJOR_21, Constants.PREVIEW_MINOR_VERSION);
-    checkVersion("Application", Constants.MAJOR_21, Constants.PREVIEW_MINOR_VERSION);
-    checkVersion("Shape", Constants.MAJOR_21, Constants.PREVIEW_MINOR_VERSION);
-    checkVersion("S", Constants.MAJOR_21, Constants.PREVIEW_MINOR_VERSION);
-  }
-
-  public void testSwitchPatternMatchingCaseLabelDominatedByPrecedingError() {
-    runTest("switch pattern matching error");
-  }
-
-  public void testSwitchPatternMatchingPreview3Error1() {
-    runTest("switch pattern matching preview 3 error 1");
-  }
-
-  public void testSwitchPatternMatchingPreview3Error2() {
-    runTest("switch pattern matching preview 3 error 2");
-  }
-
-  public void testRecordPatternsPreview1OK() {
-    // https://github.com/eclipse-jdt/eclipse.jdt.core/issues/450
-    runTest("record patterns");
-  }
-
-  public void testRecordPatternsPreview1Error() {
-    // https://github.com/eclipse-jdt/eclipse.jdt.core/issues/450 (fixed for preview 2 in Eclipse 2023-03, 4.27)
-    runTest("record patterns error");
-    checkVersion("RecordPatternsPreview1Error", Constants.MAJOR_21, Constants.PREVIEW_MINOR_VERSION);
-    checkVersion("Box", Constants.MAJOR_21, Constants.PREVIEW_MINOR_VERSION);
-  }
-
-  public void testRecordPatternsPreview1ExhaustivenessOK1() {
-    // Falsely throws 'An enhanced switch statement should be exhaustive; a default label expected' twice,
-    // see https://github.com/eclipse-jdt/eclipse.jdt.core/issues/455
-    // TODO: Remove redundant default clauses when fixed upstream
-    System.out.println("TODO: fully activate when https://github.com/eclipse-jdt/eclipse.jdt.core/issues/455 has been fixed");
-    runTest("record patterns exhaustiveness 1");
-  }
-
-  public void testRecordPatternsPreview1Aspect() {
-    runTest("record patterns aspect");
-  }
-
-  public void testRecordPatternsPreview1ExhaustivenessAspect() {
-    // Falsely throws 'An enhanced switch statement should be exhaustive; a default label expected' twice,
-    // see https://github.com/eclipse-jdt/eclipse.jdt.core/issues/455
-    // TODO: Remove redundant default clauses when fixed upstream
-    System.out.println("TODO: fully activate when https://github.com/eclipse-jdt/eclipse.jdt.core/issues/455 has been fixed");
-    runTest("record patterns exhaustiveness aspect");
-  }
-
-  public void testRecordPatternsPreview1ExhaustivenessError() {
-    // See https://github.com/eclipse-jdt/eclipse.jdt.core/issues/455
-    runTest("record patterns exhaustiveness error");
-  }
-
-  public void testRecordPatternsPreview1ExhaustivenessOK2() {
-    // Falsely throws 'An enhanced switch statement should be exhaustive; a default label expected',
-    // see https://github.com/eclipse-jdt/eclipse.jdt.core/issues/398
-    // TODO: activate when fixed
-    System.out.println("TODO: activate when https://github.com/eclipse-jdt/eclipse.jdt.core/issues/398 has been fixed");
-    //runTest("record patterns exhaustiveness 2");
+  public void testDummyPreviewJava21() {
+    //runTest("dummy preview Java 21");
   }
 
   public static Test suite() {
